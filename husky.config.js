032b8df64330cd109305bcc59c5ef70b8669fd67
Kelly Mears <developers@tinypixel.dev>
2020-08-22 05:38:42 -0700

    ci(root): remove redundant commit steps
    Commits were literally doubled up between package.json and husky

--- husky.config.js
+++ husky.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
-    'pre-commit': 'yarn run-s build lint test',
+    'pre-commit': 'yarn run-s lint build test',
     'prepare-commit-msg': 'exec < /dev/tty && git cz --hook || true',
   },
 }
