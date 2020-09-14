44586b2a17d5fca3de10aad1f4f8aeb83fb2e3ef
Kelly Mears <developers@tinypixel.dev>
2020-09-13 18:29:24 -0700

    feat(api): second param now optional in bud.copy
    affects: @roots/bud, @roots/bud-support, @roots/bud-types

--- packages/bud/src/api/copy.ts
+++ packages/bud/src/api/copy.ts
@@ -1,12 +1,12 @@
 import {Api} from '@roots/bud-types'
 import {join} from 'path'
 
-const copy: Api.Copy = function (from, to?) {
+const copy: Api.Copy = function (from, to = '') {
   this.options.set('webpack.plugins.copy.patterns', [
     ...this.options.get('webpack.plugins.copy.patterns'),
     {
       from,
-      to: to ?? join(this.paths.get('dist'), from),
+      to,
     },
   ])
 
