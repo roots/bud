3f37203829e99d48d250db675c271c4058857184
Kelly Mears <developers@tinypixel.dev>
2020-10-11 16:53:15 -0700

    style(prettier): end the prettier/eslint war
    affects: @roots/bud-babel, @roots/bud-framework

--- packages/framework/types/Model/index.d.ts
+++ packages/framework/types/Model/index.d.ts
@@ -1,25 +1,25 @@
 /**
  * CLI arguments.
  */
-export {args} from './args'
+export { args } from './args';
 /**
  * Environment variables
  */
-export {env} from './env'
+export { env } from './env';
 /**
  * Build seed configuration.
  */
-export * as build from './build'
+export * as build from './build';
 /**
  * Boolean feature flags.
  */
-export * as features from './features'
+export * as features from './features';
 /**
  * Bootstrap paths.
  */
-export * as paths from './paths'
+export * as paths from './paths';
 /**
  * Common regular expression patterns.
  */
-export * as patterns from './patterns'
+export * as patterns from './patterns';
 //# sourceMappingURL=index.d.ts.map
