da010d46a92e42124232f7f0c408ab17f3fad79d
Kelly Mears <developers@tinypixel.dev>
2020-11-01 21:00:45 -0800

    docs(hooks): document hooks
    affects: @roots/bud-framework

--- packages/framework/src/Build/builders/module/rules/post.ts
+++ packages/framework/src/Build/builders/module/rules/post.ts
@@ -3,15 +3,18 @@ import type Webpack from 'webpack'
 export default function (
   this: Framework.Bud,
 ): Webpack.RuleSetRule[] {
-  return Object.entries(this.build.rules)
-    .reduce(
-      (
-        rules: Webpack.RuleSetRule[],
-        [, rule]: [string, {make: () => Webpack.RuleSetRule}],
-      ): Webpack.RuleSetRule[] => [...rules, rule.make()],
-      [],
-    )
-    .filter(
-      ({enforce}) => enforce == 'post',
-    ) as Webpack.RuleSetRule[]
+  return this.hooks.filter(
+    `webpack.module.rules.post`,
+    Object.entries(this.build.rules)
+      .reduce(
+        (
+          rules: Webpack.RuleSetRule[],
+          [, rule]: [string, {make: () => Webpack.RuleSetRule}],
+        ): Webpack.RuleSetRule[] => [...rules, rule.make()],
+        [],
+      )
+      .filter(
+        ({enforce}) => enforce == 'post',
+      ) as Webpack.RuleSetRule[],
+  )
 }
--- packages/framework/src/Build/builders/module/rules/pre.ts
+++ packages/framework/src/Build/builders/module/rules/pre.ts
@@ -3,15 +3,18 @@ import type Webpack from 'webpack'
 export default function (
   this: Framework.Bud,
 ): Webpack.RuleSetRule[] {
-  return Object.entries(this.build.rules)
-    .reduce(
-      (
-        rules: Webpack.RuleSetRule[],
-        [, rule]: [string, {make: () => Webpack.RuleSetRule}],
-      ): Webpack.RuleSetRule[] => [...rules, rule.make()],
-      [],
-    )
-    .filter(
-      rule => rule.enforce == 'pre',
-    ) as Webpack.RuleSetRule[]
+  return this.hooks.filter(
+    `webpack.module.rules.pre`,
+    Object.entries(this.build.rules)
+      .reduce(
+        (
+          rules: Webpack.RuleSetRule[],
+          [, rule]: [string, {make: () => Webpack.RuleSetRule}],
+        ): Webpack.RuleSetRule[] => [...rules, rule.make()],
+        [],
+      )
+      .filter(
+        rule => rule.enforce == 'pre',
+      ) as Webpack.RuleSetRule[],
+  )
 }
