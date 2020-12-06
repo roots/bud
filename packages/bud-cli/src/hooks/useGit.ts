6212618ecb9436d62d4ba4b1e8122b587d95894d
Kelly Mears <developers@tinypixel.dev>
2020-12-06 14:45:57 -0800

    WIP(@roots/bud-cli): git tracking
    affects: @roots/bud-cli

--- packages/bud-cli/src/hooks/useGit.ts
+++ packages/bud-cli/src/hooks/useGit.ts
@@ -1,5 +1,4 @@
 import {useEffect, useState} from 'react'
-import {watchFile} from 'fs-extra'
 import execa from 'execa'
 
 const params = {
@@ -27,73 +26,40 @@ export const useGit: UseGit = () => {
   const [branch, setBranch] = useState(null)
   const [dirty, setDirty] = useState(null)
 
-  const [files, setFiles] = useState(null)
   const [err, setErr] = useState(false)
-  const [valid, setValid] = useState(false)
 
   useEffect(() => {
-    if (err || valid) {
-      return
+    if (err) {
+      setHead(null)
+      setBranch(null)
+      setDirty(null)
+      setStatus(null)
     }
 
     ;(async () => {
       try {
-        const {stdout: files} = await execa('git', params.files)
-        setFiles(files.split('\n'))
-      } catch (err) {
+        const {stdout: head} = await execa('git', params.head)
+        setHead(head)
+
+        const {stdout: branch} = await execa(
+          'git',
+          params.branch,
+        )
+        setBranch(branch)
+
+        const {stdout: dirty} = await execa('git', params.dirty)
+        setDirty(dirty)
+
+        const {stdout: status} = await execa(
+          'git',
+          params.status,
+        )
+        setStatus(status)
+      } catch {
         setErr(true)
       }
     })()
-  }, [files, err, valid])
-
-  useEffect(() => {
-    if (!files || err) {
-      return
-    }
-
-    const watch = async file =>
-      await watchFile(file, async () => {
-        setValid(false)
-        return true
-      })
-
-    ;(async () => {
-      await Promise.all(files.map(watch))
-    })()
-  }, [files])
-
-  useEffect(() => {
-    if (!files || err || valid) {
-      return
-    }
-
-    ;(async () => {
-      const {stdout: head} = await execa('git', params.head)
-      setHead(head)
-
-      const {stdout: branch} = await execa('git', params.branch)
-      setBranch(branch)
-
-      const {stdout: dirty} = await execa('git', params.dirty)
-      setDirty(dirty)
-
-      const {stdout: status} = await execa('git', params.status)
-      setStatus(status)
-
-      setValid(true)
-    })()
-  }, [files, err, valid])
-
-  useEffect(() => {
-    if (!err) {
-      return
-    }
-
-    setHead(null)
-    setBranch(null)
-    setDirty(null)
-    setStatus(null)
-  }, [err])
+  }, [])
 
   return {head, branch, dirty, status, err}
 }
