225347aeafa520f22fe402624d14360fbf289198
Kelly Mears <developers@tinypixel.dev>
2020-12-29 15:15:24 -0800

    chore(lab): remove lab

--- lab/localFix.js
+++
@@ -1,8 +0,0 @@
-/**
- * Resolve node_modules in local development environment
- */
-module.exports = bud =>
-  bud.hooks.on('webpack.resolve.modules', modules => [
-    ...modules,
-    bud.fs.path.join(bud.fs.base, '../node_modules'),
-  ])
