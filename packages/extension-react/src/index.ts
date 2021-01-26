b25ac5ae69cd32ba806f9af5a05d20f4299efb41
Kelly Mears <developers@tinypixel.dev>
2021-01-25 19:58:57 -0800

    [fix] react dev: add react-refresh to all entrypoints

--- packages/extension-react/src/index.ts
+++ packages/extension-react/src/index.ts
@@ -12,6 +12,16 @@ export const name = '@roots/bud-react'
  * Extension register
  */
 export const boot = (app: Bud) => {
+  app.hooks.on('webpack.entry', entry =>
+    Object.entries(entry).reduce(
+      (a, [name, assets]: [string, string[]]) => ({
+        ...a,
+        [name]: [...assets, 'react-refresh/runtime'],
+      }),
+      {},
+    ),
+  )
+
   app.babel
     .addPreset('@babel/preset-react')
     .extensions.add(
