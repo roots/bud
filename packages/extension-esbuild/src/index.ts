f336f813145c41e0a2dc46fef8409b0c3e1cc69f
Kelly Mears <developers@tinypixel.dev>
2021-02-09 13:54:06 -0800

    [fix] @roots/bud-eslint

--- packages/extension-esbuild/src/index.ts
+++ packages/extension-esbuild/src/index.ts
@@ -1,7 +1,6 @@
 import './interface'
 import {Bud} from '@roots/bud'
 import {ESBuildPlugin, ESBuildMinifyPlugin} from 'esbuild-loader'
-
 import {setOptions} from './api/setOptions'
 
 export const name = '@roots/bud-esbuild'
@@ -10,33 +9,65 @@ export const boot = (app: Bud) => {
   Object.assign(app, {
     esbuild: {
       setOptions: setOptions.bind(app),
+      tsconfigRaw: {
+        compilerOptions: {
+          importsNotUsedAsValues: 'remove',
+        },
+      },
     },
   })
 
-  app.build.set('optimization', (app: Bud) => ({
-    minimize: true,
-    minimizer: [
-      new ESBuildMinifyPlugin({
-        target: app.build.get('items.esbuild.options.target'),
-      }),
-    ],
-  }))
+  app.hooks.on('webpack.resolve.extensions', exts => [
+    ...exts,
+    '.ts',
+    '.tsx',
+  ])
 
-  app.extensions.add('esbuild', {
-    make: () => new ESBuildPlugin(),
-  })
+  app.hooks.on(
+    'webpack.devtool',
+    (devtool: Bud.Webpack.Configuration['devtool']) =>
+      devtool ?? 'cheap-source-map',
+  )
+
+  app.hooks.on(
+    'webpack.optimization',
+    (
+      optimization: Bud.Webpack.Configuration['optimization'],
+    ): Bud.Webpack.Configuration['optimization'] => ({
+      ...optimization,
+      minimizer: [
+        new ESBuildMinifyPlugin({
+          target: app.build.get('items.esbuild.options.target'),
+          exclude: app.store.get('patterns.modules'),
+        }),
+      ],
+    }),
+  )
 
   app.build
-    .set('items.esbuild', {
+    .set('items.esbuild-js', {
       loader: require.resolve('esbuild-loader'),
       options: {
         loader: 'jsx',
         target: 'es2015',
       },
     })
+    .set('items.esbuild-ts', {
+      loader: require.resolve('esbuild-loader'),
+      options: {
+        loader: 'tsx',
+        target: 'es2015',
+      },
+    })
+    .set('rules.ts', {
+      test: (app: Bud) => app.store.access('patterns.ts'),
+      use: (app: Bud) => [app.build.access('items.esbuild-ts')],
+    })
     .mutate('rules.js.use', use => [
-      app.build.access('items.cache'),
-      app.build.access('items.thread'),
-      app.build.access('items.esbuild'),
+      app.build.access('items.esbuild-js'),
     ])
+
+  app.extensions.add('esbuild', {
+    make: () => new ESBuildPlugin(),
+  })
 }
