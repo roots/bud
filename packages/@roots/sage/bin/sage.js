67ded8fd20e81c9b7743e689970288bd5089a6e6
Kelly Mears <developers@tinypixel.dev>
2021-02-27 18:35:09 -0800

    [@roots/sage] forgot to add app to sage cli constructor

--- packages/@roots/sage/bin/sage.js
+++ packages/@roots/sage/bin/sage.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
+const {sage: app} = require('../lib/cjs')
 const Bud = require('@roots/bud-cli')
 
 new Bud.CLI({
   name: 'sage',
+  app,
   projectUrl: 'https://github.com/roots/sage',
   commands: Bud.commands,
 }).invoke()
