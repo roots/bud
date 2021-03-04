10d883a9a9470206530fc0668ebdb31fe32d846c
Kelly Mears <developers@tinypixel.dev>
2021-03-04 13:09:07 -0800

    [@roots/bud] organize output of static files

--- packages/@roots/bud/src/providers/items/file.ts
+++ packages/@roots/bud/src/providers/items/file.ts
@@ -1,3 +1,8 @@
-export const file = {
+export const file = app => ({
   loader: require.resolve('file-loader'),
-}
+  options: {
+    name: app.store.enabled('options.hash')
+      ? '[ext]/[name].[hash].[ext]'
+      : '[ext]/[name].[ext]',
+  },
+})
