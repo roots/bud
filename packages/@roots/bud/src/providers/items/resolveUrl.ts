586ea4f20b3d6a2ef4505bc70983dc1f888a37e0
Kelly Mears <developers@tinypixel.dev>
2021-03-04 18:17:52 -0800

    [@roots/bud] add filters on resolve-url-loader

--- packages/@roots/bud/src/providers/items/resolveUrl.ts
+++ packages/@roots/bud/src/providers/items/resolveUrl.ts
@@ -1,7 +1,9 @@
-export const resolveUrl = {
-  loader: require.resolve('resolve-url-loader'),
-  options: {
-    root: process.cwd(),
-    sourceMap: true,
-  },
-}
+import {Framework} from '@roots/bud-framework'
+export const resolveUrl = (app: Framework) =>
+  app.hooks.filter('build.items.resolveUrl', {
+    loader: require.resolve('resolve-url-loader'),
+    options: {
+      root: app.store.get('locations.css') ?? undefined,
+      sourceMap: true,
+    },
+  })
