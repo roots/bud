eeef83915b694636a753d47f46c9fe457359b3e8
Kelly Mears <developers@tinypixel.dev>
2021-04-10 14:16:34 -0700

    update: @roots/entrypoints-webpack-plugin

--- packages/@roots/entrypoints-webpack-plugin/src/index.ts
+++ packages/@roots/entrypoints-webpack-plugin/src/index.ts
@@ -1,8 +1,10 @@
 import {RawSource} from 'webpack-sources'
 import Webpack from 'webpack'
 import {resolve, relative} from 'path'
-import {SyncWaterfallHook} from 'tapable'
 
+/**
+ * Entrypoints Webpack Plugin
+ */
 export class Plugin {
   /**
    * Plugin ident
@@ -30,7 +32,7 @@ export class Plugin {
   /**
    * Emitted filename
    */
-  public name: string
+  public name: string = 'entrypoints.json'
 
   /**
    * Emitted file path
@@ -50,139 +52,65 @@ export class Plugin {
     | Webpack.Compiler['options']['output']['publicPath']
 
   /**
-   * Where should the entrypoints manifest be emitted?
-   */
-  public outputPath: string
-
-  /**
    * Emitted contents
    */
-  public output: EntrySchema = {}
-
-  /**
-   * Should manifest be emitted
-   */
-  public writeToFileEmit: boolean
-
-  /**
-   * Class constructor
-   */
-  constructor(
-    options: Options = {
-      name: 'entrypoints.json',
-      writeToFileEmit: true,
-      publicPath: null,
-      outputPath: null,
-    },
-  ) {
-    Object.assign(this, options)
-
-    this.emit = this.emit.bind(this)
-    this.apply = this.apply.bind(this)
-    this.entrypoints = this.entrypoints.bind(this)
-  }
+  public output = {}
 
   /**
    * Webpack apply plugin
    */
   apply(compiler: Webpack.Compiler): void {
-    this.publicPath =
-      this.publicPath ?? compiler.options.output.publicPath
+    this.publicPath = compiler.options.output.publicPath
+    this.path = resolve(compiler.options.output.path, this.name)
+    this.file = relative(compiler.options.output.path, this.path)
 
-    this.outputPath =
-      this.outputPath ?? compiler.options.output.path
+    compiler.hooks.thisCompilation.tap(
+      this.plugin,
+      (compilation: Webpack.Compilation): void => {
+        compilation.hooks.processAssets.tap(
+          this.plugin,
+          assets => {
+            const raw = {}
 
-    this.path = resolve(this.outputPath, this.name)
-    this.file = relative(this.outputPath, this.path)
+            compilation.entrypoints.forEach(({name, chunks}) => {
+              chunks.map(({files}) => {
+                raw[name] = Array.from(files).reduce(
+                  (a, file) => {
+                    const type = file.split('.').pop()
 
-    compiler.hooks.emit.tapAsync(this.plugin, this.emit)
-  }
-
-  /**
-   * Emit manifest
-   */
-  async emit(
-    compilation: Webpack.Compilation,
-    callback: () => void,
-  ): Promise<void> {
-    const {
-      assets,
-      entrypoints,
-      hooks,
-      hash,
-    }: {
-      assets: Webpack.Compilation['assets']
-      entrypoints: Webpack.Compilation['entrypoints']
-      hooks: any // Webpack.Compilation['hooks']
-      hash?: Webpack.Compilation['hash']
-    } = compilation
-
-    this.hash = hash ?? null
-
-    /**
-     * @todo lazy typings @webpack5
-     */
-    hooks.entrypoints = new SyncWaterfallHook(this.hook as any)
-    hooks.entrypoints.tap(this.plugin, this.entrypoints)
-    hooks.entrypoints.call(entrypoints, this.output)
-
-    if (this.writeToFileEmit) {
-      assets[this.file] = new RawSource(
-        JSON.stringify(this.output),
-      )
-    }
-
-    callback()
-  }
-
-  /**
-   * Map entrypoints to output
-   */
-  public entrypoints(entrypoints): void {
-    try {
-      entrypoints.forEach(({name, chunks, ...entry}) => {
-        chunks.map(({files}) => {
-          this.output[name] = files.reduce(
-            (a, file) => {
-              const type = file.split('.').pop()
-
-              return {
-                ...(a ?? {}),
-                [type]: file.includes('hot-update')
-                  ? {
-                      ...(a?.[type] ?? {}),
+                    return {
+                      ...(a ?? {}),
+                      [type]: file.includes('hot-update')
+                        ? {
+                            ...(a?.[type] ?? {}),
+                          }
+                        : {
+                            ...(a?.[type] ?? {}),
+                            [name]: `${this.publicPath}${file}`,
+                          },
                     }
-                  : {
-                      ...(a?.[type] ?? {}),
-                      [name]: `${this.publicPath}${file}`,
-                    },
-              }
-            },
-
-            entry.runtimeChunk?.files.reduce(
-              (
-                a: {[key: string]: any},
-                file: {[key: string]: any},
-              ) => {
-                const type = file.split('.').pop()
-
-                return {
-                  ...(a ?? {}),
-                  [type]: {
-                    ...(a[type] ?? {}),
-                    [entry.runtimeChunk
-                      .name]: `${this.publicPath}${file}`,
                   },
-                }
-              },
-              {},
-            ),
-          )
-        })
-      })
-    } catch (err) {
-      console.error(err)
-    }
+                  {},
+                )
+
+                this.output = Object.fromEntries(
+                  Object.entries(raw).filter(item => item),
+                )
+              })
+            })
+          },
+        )
+
+        compilation.hooks.afterProcessAssets.tap(
+          this.plugin,
+          assets => {
+            assets[this.name] = new RawSource(
+              JSON.stringify(this.output),
+            )
+          },
+        )
+      },
+    )
   }
 }
 
