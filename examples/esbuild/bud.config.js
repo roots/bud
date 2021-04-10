84d194849db02524cdfcbdf895cb3c5bc86e2aa8
Kelly Mears <developers@tinypixel.dev>
2021-04-10 14:19:24 -0700

    examples

--- examples/esbuild/bud.config.js
+++ examples/esbuild/bud.config.js
@@ -7,8 +7,15 @@
 
 module.exports = app =>
   app
-    .use(require('@roots/bud-esbuild'))
+    .use([
+      require('@roots/bud-esbuild'),
+      require('@roots/bud-entrypoints'),
+      require('@roots/bud-wordpress-dependencies'),
+      require('@roots/bud-wordpress-externals'),
+      require('@roots/bud-wordpress-manifests'),
+    ])
     .html({enabled: true})
     .entry('scripts/app', 'app.js')
     .runtime()
     .splitChunks()
+    .hash()
--- examples/wordpress-theme/bud.config.js
+++ examples/wordpress-theme/bud.config.js
@@ -1,7 +1,5 @@
 module.exports = app =>
   app
-    .publicPath(app.env.get('APP_PUBLIC'))
-
     .when(
       app.isDevelopment,
       () =>
@@ -14,17 +12,14 @@ module.exports = app =>
           .use(require('@roots/bud-esbuild'))
           .esbuild.jsx()
           .hash()
-          .vendor(),
+          .splitChunks()
+          .runtime(),
     )
-
     .use([
       require('@roots/bud-postcss'),
       require('@roots/bud-entrypoints'),
-      require('@roots/bud-wordpress-externals'),
-      require('@roots/bud-wordpress-dependencies'),
-      require('@roots/bud-wordpress-manifests'),
     ])
 
     .proxy()
-    .entry('bud-app', ['app.js', 'app.css'])
-    .entry('bud-editor', ['editor.js'])
+    .entry('app', ['app.js', 'app.css'])
+    .entry('editor', ['editor.js'])
