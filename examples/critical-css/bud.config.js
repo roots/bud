bf27e264c885467f4b4a5b070d1406eab5e9bd2d
Kelly Mears <developers@tinypixel.dev>
2021-05-08 21:14:57 -0700

    examples

--- examples/critical-css/bud.config.js
+++ examples/critical-css/bud.config.js
@@ -1,22 +1,24 @@
-/**
- * ESBuild configuration example
- *
- * @typedef {import('@roots/bud').Bud} Bud
- * @type {(bud: Bud): Bud}
- */
-export default bud =>
+module.exports = bud =>
   bud
     .use([
       require('@roots/bud-entrypoints'),
       require('@roots/bud-postcss'),
       require('@roots/bud-tailwindcss'),
       require('@roots/bud-terser'),
+      require('@roots/bud-criticalcss'),
     ])
     .html({
       template: 'public/index.html',
+    })
+    .critical({
+      replace: '%INLINE_CSS%',
+      criticalOptions: {
+        html: 'public/index.html',
+      },
     })
     .hash()
     .entry('app', ['app.css'])
     .entry('app2', ['app2.css'])
     .splitChunks()
     .minimize()
+    .persist({type: 'memory'})
