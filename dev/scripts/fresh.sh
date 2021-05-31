0259d0d8c300a046f7735336d185b6deffe2b133
Kelly Mears <developers@tinypixel.dev>
2021-05-30 20:03:21 -0700

    fix: dev scripts source

--- dev/scripts/fresh.sh
+++ dev/scripts/fresh.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source './handler.sh'
+# YOINK! https://stackoverflow.com/a/4774063
+SCRIPTS_DIR="$(cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
+source "${SCRIPTS_DIR}/handler.sh"
 
 echo "Cleaning examples"
 git clean -dfx
