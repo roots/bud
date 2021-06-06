60d55784a19cadb4faf55441dc340298dd17cda9
Kelly Mears <developers@tinypixel.dev>
2021-06-06 16:31:16 -0700

    improve: dedupe definitions of items/rules/loaders

--- packages/@roots/bud-build/src/Loader/index.ts
+++ packages/@roots/bud-build/src/Loader/index.ts
@@ -1,9 +1,12 @@
-import type {Framework} from '@roots/bud-framework'
-import {Loader as Contract} from './interface'
 import {boundMethod as bind} from 'autobind-decorator'
 import {Base} from '../shared/Base'
 
-export class Loader extends Base {
+import type {
+  Framework,
+  Loader as Contract,
+} from '@roots/bud-framework'
+
+export class Loader extends Base implements Contract {
   protected src: Contract.Src
 
   public constructor(src: Contract.Input) {
--- packages/@roots/bud-build/src/Rule/index.ts
+++ packages/@roots/bud-build/src/Rule/index.ts
@@ -1,26 +1,27 @@
-import type {Framework} from '@roots/bud-framework'
-import type {Rule as Contract} from './interface'
-import type {Item} from '../Item/interface'
 import {boundMethod as bind} from 'autobind-decorator'
 import {isFunction} from 'lodash'
 
+import type {
+  Framework,
+  Rule as Contract,
+  Item,
+} from '@roots/bud-framework'
+
 class Rule implements Contract {
-  protected test: Contract.TestFn
-  protected use: Contract.UseFn
-  protected exclude: Contract.ExcludeFn
-  protected type: Contract.TypeFn
+  public test: Contract.TestFn
+
+  public use: Contract.UseFn
+
+  public exclude: Contract.ExcludeFn
+
+  public type: Contract.TypeFn
 
   public constructor({
     test,
     use = null,
     exclude = null,
     type = null,
-  }: {
-    test: RegExp | Contract.TestFn
-    use?: Item[] | Contract.UseFn
-    exclude?: RegExp | Contract.ExcludeFn
-    type?: string | Contract.TypeFn
-  }) {
+  }: Contract.Options) {
     this.test = isFunction(test) ? test : () => test
 
     if (use) {
@@ -61,12 +62,12 @@ class Rule implements Contract {
   }
 
   @bind
-  public getExclude(app: Framework) {
+  public getExclude(app: Framework): Contract.Output['exclude'] {
     return this.exclude ? this.exclude(app) : null
   }
 
   @bind
-  public setExclude(exclude: (app: Framework) => RegExp) {
+  public setExclude(exclude: (app: Framework) => RegExp): void {
     this.exclude = exclude
   }
 
@@ -82,15 +83,7 @@ class Rule implements Contract {
 
   @bind
   public make(app: Framework) {
-    const output: {
-      test: RegExp
-      use?: {
-        loader: string
-        options?: {[key: string]: any}
-      }[]
-      exclude?: RegExp
-      type?: string
-    } = {
+    const output: Contract.Output = {
       test: this.test(app),
     }
 
