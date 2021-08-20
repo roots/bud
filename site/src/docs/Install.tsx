fe0608dfed0756a1863165a364b9c989a23c7d9a
Kelly Mears <developers@tinypixel.dev>
2021-08-20 03:09:21 -0700

    docs

---
+++ site/src/docs/Install.tsx
@@ -0,0 +1,27 @@
+import CodeBlock from '@theme/CodeBlock'
+import TabItem from '@theme/TabItem'
+import Tabs from '@theme/Tabs'
+import React from 'react'
+
+const Install = ({packages}: {packages: string}) => (
+  <Tabs
+    defaultValue="yarn"
+    groupId="pacman"
+    values={[
+      {label: 'yarn', value: 'yarn'},
+      {label: 'npm', value: 'npm'},
+    ]}>
+    <TabItem value="yarn">
+      <CodeBlock className="language-shell">
+        {`yarn add ${packages} --dev`}
+      </CodeBlock>
+    </TabItem>
+    <TabItem value="npm">
+      <CodeBlock className="language-shell">
+        {`npm install ${packages} --save-dev`}
+      </CodeBlock>
+    </TabItem>
+  </Tabs>
+)
+
+export {Install}
