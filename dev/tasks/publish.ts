7a8da4895f29fe1dfe026ed761b8d83459e43f12
Kelly Mears <developers@tinypixel.dev>
2021-08-23 23:00:32 -0700

    add: publish script

---
+++ dev/tasks/publish.ts
@@ -0,0 +1,30 @@
+import {globby} from '@roots/bud-support'
+import * as execa from 'execa'
+
+const tag = process.argv.splice(2).pop()
+
+/**
+ * Run publish
+ */
+;(async () => {
+  const pkgs = await globby.globby('packages/@roots/*', {
+    absolute: false,
+    cwd: process.cwd(),
+    onlyFiles: false,
+  })
+
+  pkgs.map((pkg: string) => {
+    const command = [
+      'npm',
+      'publish',
+      '--tolerate-republish',
+      '--access',
+      'public',
+    ]
+    if (tag) {
+      command.push('--tag', tag)
+    }
+
+    execa.sync('yarn', command, {cwd: pkg})
+  })
+})()
