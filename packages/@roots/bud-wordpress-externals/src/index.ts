430e92f0c70b39f2e9c9d9630b74295e2e208f41
Kelly Mears <developers@tinypixel.dev>
2021-08-25 01:17:33 -0700

    fix: type resolution

--- packages/@roots/bud-wordpress-externals/src/index.ts
+++ packages/@roots/bud-wordpress-externals/src/index.ts
@@ -1,10 +1,16 @@
-import './interface'
-
 import type {Module} from '@roots/bud-framework'
 import {
   Plugin,
   WordPressExternals,
 } from '@roots/wordpress-externals-webpack-plugin'
+
+declare module '@roots/bud-framework' {
+  namespace Framework {
+    interface Extensions {
+      '@roots/wordpress-externals-webpack-plugin': Module
+    }
+  }
+}
 
 export const name: Module['name'] =
   '@roots/wordpress-externals-webpack-plugin'
