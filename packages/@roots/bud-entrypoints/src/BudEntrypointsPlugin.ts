677339a2d1ab92a132cb7ce4aa4b3d29f85e7578
Kelly Mears <developers@tinypixel.dev>
2021-08-28 04:46:33 -0700

    fix: missing module

--- packages/@roots/bud-entrypoints/src/BudEntrypointsPlugin.ts
+++ packages/@roots/bud-entrypoints/src/BudEntrypointsPlugin.ts
@@ -1,6 +1,9 @@
+import {WebpackPlugin} from '@roots/bud-framework'
 import {EntrypointsWebpackPlugin} from '@roots/entrypoints-webpack-plugin'
 
-const BudEntrypointsPlugin = {
+interface BudEntrypointsPlugin extends WebpackPlugin {}
+
+const BudEntrypointsPlugin: BudEntrypointsPlugin = {
   name: '@roots/bud-entrypoints',
   make: () => new EntrypointsWebpackPlugin(),
 }
