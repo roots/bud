45435602170013df5c23039cb9c33a40d2f2a91d
Kelly Mears <developers@tinypixel.dev>
2021-09-03 19:59:44 -0700

    tests & fixes: project refs

--- tests/unit/root/index.ts
+++ tests/unit/root/index.ts
@@ -1,8 +1,165 @@
-import {readFile} from 'fs-extra'
+import {globby} from '@roots/bud-support'
+import {readFile, readJsonSync} from 'fs-extra'
 import {join} from 'path'
 
 describe('repo', function () {
-  it('LICENSE.md matches snapshot', async () => {
+  describe('publish check: */lib/cjs/index.js', () => {
+    globby
+      .globbySync('packages/@roots/*', {
+        absolute: true,
+        onlyDirectories: true,
+      })
+      .map(pkg => {
+        const name = pkg.split(`packages/`).pop()
+
+        it(`${name} has cjs`, done => {
+          const cjs = globby.globbySync(
+            pkg.concat('/lib/cjs/index.js'),
+          )
+
+          expect(cjs.length).toBe(1)
+          done()
+        })
+      })
+  })
+
+  describe('publish check: */lib/esm/index.js', () => {
+    globby
+      .globbySync('packages/@roots/*', {
+        absolute: true,
+        onlyDirectories: true,
+      })
+      .map(pkg => {
+        const name = pkg.split(`packages/`).pop()
+
+        it(`${name} has esm`, done => {
+          const esm = globby.globbySync(
+            pkg.concat('/lib/esm/index.js'),
+          )
+
+          expect(esm.length).toBe(1)
+          done()
+        })
+      })
+  })
+
+  describe('publish check: */LICENSE.md', () => {
+    globby
+      .globbySync('packages/@roots/*', {
+        absolute: true,
+        onlyDirectories: true,
+      })
+      .map(pkg => {
+        const name = pkg.split(`packages/`).pop()
+
+        it(`${name} has LICENSE.md`, done => {
+          const esm = globby.globbySync(
+            pkg.concat('/LICENSE.md'),
+          )
+
+          expect(esm.length).toBe(1)
+          done()
+        })
+      })
+  })
+
+  describe('publish check: */README.md', () => {
+    globby
+      .globbySync('packages/@roots/*', {
+        absolute: true,
+        onlyDirectories: true,
+      })
+      .map(pkg => {
+        const name = pkg.split(`packages/`).pop()
+
+        it(`${name} has README.md`, done => {
+          const esm = globby.globbySync(pkg.concat('/README.md'))
+
+          expect(esm.length).toBe(1)
+          done()
+        })
+      })
+  })
+
+  describe('publish check: types', () => {
+    globby
+      .globbySync('packages/@roots/*', {
+        absolute: true,
+        onlyDirectories: true,
+      })
+      .map(pkg => {
+        const name = pkg.split(`packages/`).pop()
+
+        it(`${name} esm types`, done => {
+          const esm = globby.globbySync(
+            pkg.concat('/types/index.d.ts'),
+          )
+
+          expect(esm.length).toBe(1)
+          done()
+        })
+      })
+  })
+
+  describe('publish check: project references', () => {
+    globby
+      .globbySync('packages/@roots/*', {
+        absolute: true,
+        onlyDirectories: true,
+      })
+      .map(pkg => {
+        const name = pkg.split(`packages/`).pop()
+
+        const pkgJson = readJsonSync(pkg.concat('/package.json'))
+        const esmJson = readJsonSync(
+          pkg.concat('/tsconfig-esm.json'),
+        )
+        const cjsJson = readJsonSync(
+          pkg.concat('/tsconfig.json'),
+        )
+
+        const workspaceDeps = Object.keys({
+          ...(pkgJson.dependencies ?? {}),
+          ...(pkgJson.devDependencies ?? {}),
+        }).filter(k => k.includes('@roots/'))
+
+        workspaceDeps.map(dependency => {
+          it(`[${name}] ${dependency} has a ts reference (cjs)`, done => {
+            const refPath = `${dependency
+              .split('@roots/')
+              .pop()}/tsconfig.json`
+
+            expect(
+              cjsJson.references.filter(
+                ({path}: {path: string}) => {
+                  return path.includes(refPath)
+                },
+              ).length,
+            ).toEqual(1)
+
+            done()
+          })
+
+          it(`[${name}] ${dependency} has a ts reference (esm)`, done => {
+            const refPath = `${dependency
+              .split('@roots/')
+              .pop()}/tsconfig-esm.json`
+
+            expect(
+              esmJson.references.filter(
+                ({path}: {path: string}) => {
+                  return path.includes(refPath)
+                },
+              ).length,
+            ).toEqual(1)
+
+            done()
+          })
+        })
+      })
+  })
+
+  it('root: LICENSE.md matches snapshot', async () => {
     const artifact = await readFile(
       join(process.cwd(), 'LICENSE.md'),
     )
@@ -10,11 +167,58 @@ describe('repo', function () {
     expect(artifact.toString()).toMatchSnapshot()
   })
 
-  it('sass is not installed in the monorepo', async () => {
-    try {
-      expect(require.resolve('sass')).toThrowError(
-        `Cannot find module 'sass' from 'tests/unit/root/index.ts`,
-      )
-    } catch {}
+  describe('root: project references', () => {
+    const tsConfCjs = readJsonSync(
+      process.cwd().concat('/tsconfig.json'),
+    )
+    const tsConfEsm = readJsonSync(
+      process.cwd().concat('/tsconfig.esm.json'),
+    )
+
+    globby
+      .globbySync('packages/@roots/*', {
+        absolute: true,
+        onlyDirectories: true,
+      })
+      .map(pkg => {
+        const name = pkg
+          .split(`packages/`)
+          .pop()
+          .concat('/tsconfig.json')
+
+        it(`has cjs ref for ${name}`, done => {
+          expect(
+            tsConfCjs.references.filter(
+              ({path}: {path: string}) => {
+                return path.includes(name)
+              },
+            ).length,
+          ).toBe(1)
+          done()
+        })
+      })
+
+    globby
+      .globbySync('packages/@roots/*', {
+        absolute: true,
+        onlyDirectories: true,
+      })
+      .map(pkg => {
+        const name = pkg
+          .split(`packages/`)
+          .pop()
+          .concat('/tsconfig-esm.json')
+
+        it(`has esm ref for ${name}`, done => {
+          expect(
+            tsConfEsm.references.filter(
+              ({path}: {path: string}) => {
+                return path.includes(name)
+              },
+            ).length,
+          ).toBe(1)
+          done()
+        })
+      })
   })
 })
