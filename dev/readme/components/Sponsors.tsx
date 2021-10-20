ac2c12b59fe632c4345b96f34632ae4502cb7aa3
Kelly Mears <developers@tinypixel.dev>
2021-10-20 03:45:32 -0700

    dev: update deprecated prettier rule

--- dev/readme/components/Sponsors.tsx
+++ dev/readme/components/Sponsors.tsx
@@ -3,9 +3,8 @@ import * as React from 'react'
 const Grid = ({project}) =>
   project.sponsors.map((sponsor, id) => (
     <span
-      key={
-        id
-      }>{`<a href="${sponsor.url}"><img src="${sponsor.image}" alt="${sponsor.title}" width="200" height="150"/></a>`}</span>
+      key={id}
+    >{`<a href="${sponsor.url}"><img src="${sponsor.image}" alt="${sponsor.title}" width="200" height="150"/></a>`}</span>
   ))
 
 export const Sponsors = ({project}) => (
