a22d5c746e5d74b1a0b11bf46dab1dd2714e3fb0
Kelly Mears <developers@tinypixel.dev>
2021-10-21 13:26:14 -0700

    examples: bedrock

--- examples/bedrock/bud.config.js
+++ examples/bedrock/bud.config.js
@@ -2,23 +2,22 @@ const path = require('path')
 
 module.exports = bud => {
   bud
-    /**
-     * Build theme
-     */
-    .make('sage', sage => {
-      sage
-        .use([
-          require('@roots/bud-preset-wordpress'),
-          require('@roots/bud-eslint'),
-          require('@roots/bud-tailwindcss'),
-        ])
-        .setPath('project', path.resolve('web/app/themes/sage'))
-        .setPath('src', 'resources')
-        .setPath('dist', 'public')
-        .entry({
-          app: '**/app.{js,css}',
-          editor: '**/editor.{js,css}',
-        })
+    .use([
+      require('@roots/bud-preset-wordpress'),
+      require('@roots/bud-eslint'),
+      require('@roots/bud-tailwindcss'),
+    ])
+    .setPath({
+      project: path.resolve('web/app/themes/sage'),
+      src: 'resources',
+      dist: 'public',
+    })
+    .entry({
+      app: '**/app.{js,css}',
+      editor: '**/editor.{js,css}',
+    })
+    .when(bud.isDevelopment, () => {
+      bud
         .dev({
           host: 'http://bedrock.test',
           port: 3000,
