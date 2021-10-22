4aeceac59feacce7a3fe4981f69a00137cbc895e
Kelly Mears <developers@tinypixel.dev>
2021-10-22 02:47:24 -0700

    improve: bud.minimize

---
+++ packages/@roots/bud-api/src/Repository/minimize/minimize.dependencies.ts
@@ -0,0 +1,6 @@
+import {safeRequire} from '@roots/bud-support'
+
+const CssMinimizerWebpackPlugin = safeRequire(
+  'css-minimizer-webpack-plugin',
+)
+export const CssMinimizer = CssMinimizerWebpackPlugin
