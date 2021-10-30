74022a583eb6545ab8d575666c7caf4e4d182b43
Kelly Mears <developers@tinypixel.dev>
2021-10-30 00:33:25 -0700

    dev: setup script

--- dev/jest/jest.setup.js
+++ dev/jest/jest.setup.js
@@ -10,60 +10,55 @@ const {readFileSync, writeFile} = require('fs-extra')
 const clearBudfiles = require('./util/clearBudfiles')
 const {install} = require('./util/dependencies')
 
+const IGNORE_LIST = [
+  'examples/bedrock/package.json',
+  'examples/node-api/package.json',
+  'examples/multi-compiler/package.json',
+  'examples/sass/package.json',
+  'examples/basic/package.json',
+  'examples/preset-recommend/package.json',
+  'examples/typescript/package.json',
+]
+
 module.exports = async () => {
   await clearBudfiles()
   await install()
 
-  const raw = await globby('examples/*/package.json')
-  const paths = raw
-    .filter(
-      path =>
-        !path.includes('examples/bedrock/package.json') &&
-        !path.includes('examples/node-api/package.json') &&
-        !path.includes('examples/multi-compiler/package.json'),
-    )
-    .map(s => ({
-      manifest: s,
-      manifestStr: readFileSync(s),
-      cwd: process.cwd().concat(`/${dirname(s)}`),
+  const search = await globby(`examples/*/package.json`)
+  const paths = search
+    .filter(IGNORE_LIST.includes)
+    .map(manifest => ({
+      manifest: manifest,
+      manifestStr: readFileSync(manifest),
+      cwd: process.cwd().concat(`/${dirname(manifest)}`),
     }))
-    .map(s => ({
-      ...s,
-      name: s.cwd.split('examples/')[1],
+    .map(manifest => ({
+      ...manifest,
+      name: manifest.cwd.split('examples/')[1],
     }))
 
-  await paths.reduce(async (promised, {name, cwd}) => {
-    await promised
+  await Promise.all(
+    paths.map(async ({name, cwd}) => {
+      console.log(
+        `\n${bold.underline`${name}`}\npath: ${cwd}\n${blue`\n${name} Installing`}`,
+      )
 
-    console.log('\n ', bold.underline`${name}`)
-    console.log(`  path: ${cwd}`)
+      const install = execa.command(`yarn bud init`, {cwd})
+      install.stdout.pipe(process.stdout)
+      install.stderr.pipe(process.stderr)
+      await install.finally(noop)
 
-    console.log(blue`\n ${name} Installing`)
+      const build = execa.command(`yarn bud build`, {cwd})
+      build.stdout.pipe(process.stdout)
+      build.stderr.pipe(process.stderr)
+      await build.finally(noop)
+    }),
+  )
 
-    const install = execa.command(`yarn bud init`, {cwd})
-    install.stdout.pipe(process.stdout)
-    install.stderr.pipe(process.stderr)
-    await install.finally(noop)
-
-    const build = execa.command(`yarn bud build --log --ci`, {
-      cwd,
-    })
-    build.stdout.pipe(process.stdout)
-    build.stderr.pipe(process.stderr)
-    await build.finally(noop)
-
-    return Promise.resolve({})
-  }, Promise.resolve({}))
-
-  await paths.reduce(
-    async (promised, {name, manifest, manifestStr}) => {
-      await promised
-
-      console.log(blue`\n ${name} Restoring manifest`)
+  await Promise.all(
+    paths.map(async ({name, manifest, manifestStr}) => {
+      console.log(blue`\n${name} Restoring manifest`)
       await writeFile(manifest, manifestStr)
-
-      return Promise.resolve({})
-    },
-    Promise.resolve({}),
+    }),
   )
 }
