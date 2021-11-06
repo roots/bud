97bb4aa42dc980e9ad756ea54905da13cd402a2a
Kelly Mears <developers@tinypixel.dev>
2021-11-06 07:59:00 -0700

    fix: tests

---
+++ dev/jest/jest.all.js
@@ -0,0 +1,37 @@
+// @ts-check
+
+const mapModuleNames = require('./util/moduleNameMapper')
+
+module.exports = async () => {
+  const moduleNameMapper = await mapModuleNames()
+
+  return {
+    name: 'all',
+    displayName: {
+      name: 'all',
+      color: 'blue',
+    },
+    globals: {
+      'ts-jest': {
+        tsconfig: '<rootDir>/dev/jest/tsconfig.jest.json',
+        compiler: 'typescript',
+      },
+    },
+    moduleNameMapper,
+    rootDir: process.cwd(),
+    globalSetup: '<rootDir>/dev/jest/util/setup.js',
+    globalTeardown: '<rootDir>/dev/jest/util/teardown.js',
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: [
+      `<rootDir>/tests/unit/**/*.ts`,
+      `<rootDir>/tests/integration/**/*.ts`,
+      `!**/__mocks__/**/*`,
+    ],
+    testPathIgnorePatterns: [
+      '/node_modules/',
+      '/tests/util/',
+      '/tests/.*?/__mocks__/',
+    ],
+  }
+}
