c094544ece6904912fd1d5ea8db738278a63bad7
QWp6t <hi@qwp6t.me>
2021-11-09 02:04:38 -0800

    fix(test-babel): i have no idea what i'm doing but it worked

--- examples/babel/bud.config.js
+++ examples/babel/bud.config.js
@@ -8,32 +8,35 @@
 /**
  * @param {Bud} app
  */
-module.exports = app => {
-  app.babel
-    .setPresets({
-      '@babel/preset-env': require.resolve('@babel/preset-env'),
-    })
-    .setPlugins({
-      '@babel/plugin-transform-runtime': [
-        require.resolve('@babel/plugin-transform-runtime'),
-        {helpers: false},
-      ],
-      '@babel/plugin-proposal-object-rest-spread':
-        require.resolve(
-          '@babel/plugin-proposal-object-rest-spread',
-        ),
-      '@babel/plugin-syntax-dynamic-import': require.resolve(
-        '@babel/plugin-syntax-dynamic-import',
-      ),
-
-      '@babel/plugin-proposal-class-properties': require.resolve(
-        '@babel/plugin-proposal-class-properties',
-      ),
-    })
-
+module.exports = app =>
   app
+    .tap(({babel}) =>
+      babel
+        .setPresets({
+          '@babel/preset-env': require.resolve(
+            '@babel/preset-env',
+          ),
+        })
+        .setPlugins({
+          '@babel/plugin-transform-runtime': [
+            require.resolve('@babel/plugin-transform-runtime'),
+            {helpers: false},
+          ],
+          '@babel/plugin-proposal-object-rest-spread':
+            require.resolve(
+              '@babel/plugin-proposal-object-rest-spread',
+            ),
+          '@babel/plugin-syntax-dynamic-import': require.resolve(
+            '@babel/plugin-syntax-dynamic-import',
+          ),
+
+          '@babel/plugin-proposal-class-properties':
+            require.resolve(
+              '@babel/plugin-proposal-class-properties',
+            ),
+        }),
+    )
     .template()
     .entry('app', '*.{js,css}')
     .splitChunks()
     .minimize()
-}
