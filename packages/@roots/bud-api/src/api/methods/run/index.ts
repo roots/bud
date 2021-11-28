6fbd5ebc3338f904b2516e453abaae5e041ff806
Kelly Mears <developers@tinypixel.dev>
2021-11-27 18:05:24 -0800

    improve: double check api/extensions queues are cleared before run

--- packages/@roots/bud-api/src/api/methods/run/index.ts
+++ packages/@roots/bud-api/src/api/methods/run/index.ts
@@ -1,26 +1,31 @@
 import type {Framework} from '@roots/bud-framework'
 
 export interface run {
-  (this: Framework): Promise<void>
+  (): Promise<void>
 }
 
 export const run: run = async function (): Promise<void> {
-  this.hooks.filter('run', this)
+  this as Framework
+
+  await this.extensions.processQueue()
+  await this.api.processQueue()
+
+  this.hooks.promised('run', this)
 
   const isDev =
     this.isDevelopment &&
     this.server?.run &&
     this.store.is('server.middleware.hot', true)
 
-  const dev = async () => {
+  const development = async () => {
     this.server.inject()
     await this.server.run()
   }
 
-  const prod = async () => {
+  const production = async () => {
     const compiler = await this.compiler.compile()
     compiler.run(this.compiler.callback)
   }
 
-  isDev ? await dev() : await prod()
+  isDev ? await development() : await production()
 }
