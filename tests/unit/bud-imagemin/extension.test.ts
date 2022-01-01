c0c52d9752673025dd1da4588cd84fed60590760
Kelly Mears <developers@tinypixel.dev>
2021-12-31 17:45:09 -0800

    test(imagemin) update test

--- tests/unit/bud-imagemin/extension.test.ts
+++ tests/unit/bud-imagemin/extension.test.ts
@@ -3,7 +3,8 @@ import * as imagemin from '@roots/bud-imagemin'
 describe('@roots/bud-imagemin', () => {
   it('has expected exports', () => {
     expect(imagemin.name).toBe('@roots/bud-imagemin')
-    expect(imagemin.register).toBeInstanceOf(Function)
+    // @ts-ignore
+    expect(imagemin.api.imagemin).toBeInstanceOf(Function)
     expect(imagemin.boot).toBeInstanceOf(Function)
   })
 })
