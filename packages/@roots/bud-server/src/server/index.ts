75750854f0daf70d932941c4b92e78b7efd31ed4
Kelly Mears <developers@tinypixel.dev>
2022-01-01 17:44:20 -0800

    improve(server) filterable injection, middleware

--- packages/@roots/bud-server/src/server/index.ts
+++ packages/@roots/bud-server/src/server/index.ts
@@ -1,12 +1,11 @@
 import * as Framework from '@roots/bud-framework'
 import Express from 'express'
-import {URL} from 'url'
 
 import * as middleware from '../middleware'
 import __BudRouter from '../router/__bud'
 import {inject} from '../util/inject'
-import {bind, chokidar, globby} from './server.dependencies'
-import type {Watcher} from './server.interface'
+import {bind} from './server.dependencies'
+import {Watcher} from './server.watcher'
 
 /**
  * Server service class
@@ -45,7 +44,7 @@ export class Server
    *
    * @public
    */
-  public middleware: Framework.Server.Middleware = {}
+  public middleware: Framework.Server.Middleware
 
   /**
    * Watcher instance
@@ -55,63 +54,39 @@ export class Server
   public watcher: Watcher
 
   /**
-   * Service register callback
+   * Port
    *
-   * @internal
+   * @public
    */
-  public async bootstrap(): Promise<void> {
+  public get port(): string {
+    const url = this.app.store.get('server.dev.url')
+    if (!url.port || url.port == '') {
+      return url.protocol == 'https' ? '443' : '80'
+    }
+
+    return url.port
+  }
+
+  /**
+   * Service boot callback
+   *
+   * @public
+   * @decorator `@bind`
+   */
+  @bind
+  public async boot(): Promise<void> {
     this.application = Express()
-  }
 
-  /**
-   * {@inheritDoc @roots/bud-framework#Server.Interface.getWatchedFilesArray}
-   *
-   * @public
-   * @decorator `@bind`
-   */
-  @bind
-  public async getWatchedFiles(): Promise<Array<string>> {
-    const [files, options] =
-      this.app.store.getValues('server.watch')
+    this.app.hooks.on('server.inject', () => [
+      instance =>
+        `@roots/bud-server/client/index.js?name=${instance.name}&path=/__bud/hmr`,
+      () =>
+        `@roots/bud-server/client/proxy-click-interceptor.js`,
+    ])
 
-    if (!files?.length) return []
-
-    const globResults = await globby(
-      files.map((file: string) =>
-        this.app.path('project', file),
-      ),
-      options,
-    )
-
-    return globResults as unknown as Array<string>
-  }
-
-  /**
-   * Process middlewares
-   *
-   * @public
-   * @decorator `@bind`
-   */
-  @bind
-  public processMiddlewares() {
-    this.middleware = Object.entries(middleware)
-      .filter(([k, v]) => {
-        if (this.app.store.is(`server.middleware.${k}`, true)) {
-          this.log('log', `middleware ${k} is enabled.`)
-          return true
-        }
-
-        return false
-      })
-      .reduce((acc, [key, generate]) => {
-        const middleware = generate(this.app)
-
-        this.log(`info`, `configured middleware: ${key}`)
-
-        this.application.use(middleware)
-
-        return {...acc, [key]: middleware}
-      }, {})
+    this.app.hooks.on('server.middleware', () => {
+      return middleware
+    })
   }
 
   /**
@@ -122,18 +97,87 @@ export class Server
    */
   @bind
   public async run(): Promise<this> {
+    /**
+     * Instantiate watcher
+     */
+    this.watcher = new Watcher(this.app)
+
+    /**
+     * Filter server before
+     */
     await this.app.hooks.filterAsync<'event.server.before'>(
       'event.server.before',
       this.app,
     )
 
+    /**
+     * Prep and run compilation
+     */
+    await this.compile()
+
+    /**
+     * Apply middleware
+     */
+    Object.entries(
+      this.app.hooks.filter('server.middleware'),
+    ).map(([key, factory]) => {
+      this.log(`info`, `using middleware: ${key}`)
+      this.application.use(factory(this.app))
+    })
+
+    /**
+     * 404 middleware
+     */
+    this.application.use(
+      (
+        _req: Express.Request,
+        res: Express.Response,
+        _next: Express.NextFunction,
+      ) => {
+        res.status(404).send("Sorry can't find that!")
+      },
+    )
+
+    /**
+     * Listen
+     */
+    this.instance = this.application.listen(
+      this.port,
+      async (error: string) => {
+        this.log('info', `started server on %s`, this.port)
+        if (error) this.log('error', error)
+
+        this.app.hooks.filter('event.server.listen')
+      },
+    )
+
+    /**
+     * If watching and a file it is watching
+     * is touched, reload the window.
+     */
+    this.instance.on('change', path => {
+      this.middleware?.hot?.publish({
+        action: 'reload',
+        message: `Detected file change: ${path}. Reloading window.`,
+      })
+    })
+
+    await this.app.hooks.filterAsync<'event.server.after'>(
+      'event.server.after',
+      this.app,
+    )
+
+    return this
+  }
+
+  @bind
+  public async compile() {
     await Promise.all(
       [this.app, ...this.app.children.getValues()].map(
         async (instance: Framework.Framework) => {
           await inject(
             instance,
-            (instance: Framework.Framework) =>
-              `@roots/bud-server/client/index.js?name=${instance.name}&path=/__bud/hmr`,
+            this.app.hooks.filter('server.inject'),
           )
         },
       ),
@@ -147,67 +191,10 @@ export class Server
     })
 
     await this.app.compiler.compile()
-    this.processMiddlewares()
-
-    this.application.use((req, res, next) => {
-      res.status(404).send("Sorry can't find that!")
-    })
-
-    /**
-     * Listen
-     */
-    const url = new URL(this.app.store.get('server.dev.url'))
-    this.log('info', `starting server on %s`, url.port)
-
-    this.instance = this.application.listen(
-      url.port,
-      async (error: string) => {
-        if (error) this.log('error', error)
-
-        this.app.hooks.filter('event.server.listen')
-
-        this.log(
-          'success',
-          'listening on %s',
-          JSON.stringify(this.instance.address()),
-        )
-      },
-    )
-
-    /**
-     * Initialize Watcher
-     */
-    const watchFiles = await this.getWatchedFiles()
-
-    if (watchFiles.length) {
-      watchFiles.forEach(file => {
-        this.log('log', `watching`, file, `for changes`)
-      })
-      this.watcher = chokidar.watch(watchFiles)
-    }
-
-    /**
-     * If watching and a file it is watching
-     * is touched, reload the window.
-     */
-    this.watcher?.on &&
-      this.watcher.on('change', path => {
-        this.middleware?.hot?.publish({
-          action: 'reload',
-          message: `Detected file change: ${path}. Reloading window.`,
-        })
-      })
-
-    await this.app.hooks.filterAsync<'event.server.after'>(
-      'event.server.after',
-      this.app,
-    )
-
-    return this
   }
 
   /**
-   * {@inheritDoc @roots/bud-framework#Server.Interface.inject}
+   * App close handler
    *
    * @public
    * @decorator `@bind`
