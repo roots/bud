eaf7818bbfcc417ad94df1f6fdb307260c8dd939
Kelly Mears <developers@tinypixel.dev>
2022-01-06 02:47:23 -0800

    fix(sage) remove unncessary URL wrap in hmr.json

--- packages/@roots/sage/src/hooks/event.compiler.done.ts
+++ packages/@roots/sage/src/hooks/event.compiler.done.ts
@@ -1,21 +1,17 @@
 import {Framework} from '@roots/bud-framework'
 import {fs} from '@roots/bud-support'
 import {ensureDir} from 'fs-extra'
-import {URL, urlToHttpOptions} from 'url'
+import {urlToHttpOptions} from 'url'
 import {Stats} from 'webpack'
 
 const {writeJson} = fs
 
 export default (app: Framework) => async (stats: Stats) => {
   const contents = {
-    dev: urlToHttpOptions(
-      new URL(app.store.get('server.dev.url')),
-    ),
-    proxy: app.store.is('server.middleware.proxy', true)
-      ? urlToHttpOptions(
-          new URL(app.store.get('server.proxy.url')),
-        )
-      : false,
+    dev: urlToHttpOptions(app.store.get('server.dev.url')),
+    proxy:
+      urlToHttpOptions(app.store.get('server.proxy.url')) ??
+      false,
     publicPath: app.hooks.filter('build.output.publicPath'),
   }
 
