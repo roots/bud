e386ca7b55c6136ff1f750b441930de214c8b071
Kelly Mears <developers@tinypixel.dev>
2022-01-09 06:03:25 -0800

    proxy/docker works-ish

--- dev/docker/bud/init.sh
+++ dev/docker/bud/init.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
--e
-
-npm install yarn --global
+set -e
 
 cd /bud
 
 yarn @bud make
-yarn @bud proxy make
-yarn @bud proxy publish --version 0.0.0
-
-source /npm/test.sh
-source /yarn/test.sh
 
 while true; do
   sleep 100
