5b8688b5cbfd9d4df47d7221789322c31d7ffbdc
Kelly Mears <developers@tinypixel.dev>
2022-01-19 06:31:43 -0800

    fix(dev) github workflow git auth (#1000)
    * just one command for all releases
    
    * move proxy wipe before install

---
+++ sources/yarn-plugin-bud/sources/command/release.command.ts
@@ -0,0 +1,203 @@
+import {CommandClass, Option} from 'clipanion'
+import {readJson, remove, writeJson} from 'fs-extra'
+import {parse} from 'semver'
+import {ROOTS_PATH} from '../constants'
+
+import {Command} from './base.command'
+
+/**
+ * Execution steps
+ *
+ * @internal
+ */
+export type EXECUTION_STEPS = 'preflight' | 'bump' | 'make' | 'publish'
+
+/**
+ * Release command
+ *
+ * @internal
+ */
+export class Release extends Command {
+  /**
+   * Command name
+   *
+   * @internal
+   */
+  public name = 'release npm'
+
+  /**
+   * --version flag
+   *
+   * @internal
+   */
+  public version = Option.String(`-v,--version`, null, {
+    description: `version`,
+  })
+
+  /**
+   * --tag flag
+   *
+   * @internal
+   */
+  public tag = Option.String(`-t,--tag`, null, {
+    description: `tag`,
+  })
+
+  /**
+   * Command paths
+   *
+   * @internal
+   */
+  public static paths: CommandClass['paths'] = [[`@bud`, `release`]]
+
+  /**
+   * Command usage
+   *
+   * @internal
+   */
+  public static usage: CommandClass['usage'] = {
+    category: `@bud`,
+    description: `Do a release.`,
+    examples: [
+      [
+        `yarn @bud release --version x.y.z --tag latest`,
+        `Bump packages to x.y.z and publish`,
+      ],
+    ],
+  }
+
+  /**
+   * Returns true if tagged `latest`
+   *
+   * @internal
+   */
+  public isTaggedLatest() {
+    return this.tag === 'latest'
+  }
+
+  /**
+   * Execute command
+   *
+   * @remarks
+   * You must be in the roots staff channel to see this link. It is
+   * just a broader overview of the steps.
+   *
+   * @internal
+   */
+  public async execute() {
+    await this.executeStep(`preflight`)
+    await this.executeStep(`bump`)
+    await this.executeStep(`make`)
+    await this.executeStep(`publish`)
+  }
+
+  /**
+   * Execute a step
+   *
+   * @remarks
+   * This is a helper function to execute a step.
+   * It will throw an error and reset yarnrc state if the step fails.
+   *
+   * @param step - the step to execute
+   *
+   * @internal
+   */
+  public async executeStep(step: EXECUTION_STEPS) {
+    try {
+      if (!this[step]) throw new Error(`${step} is not implemented`)
+
+      await this[`${step}`]()
+    } catch (error) {
+      this.err(`${step} failed: ${error.message}\n`)
+      this.err(error.stack)
+
+      process.exit(1)
+    }
+  }
+
+  /**
+   * Check tag validity
+   *
+   * @internal
+   */
+  public async checkTagIsValid() {
+    if (!this.tag) {
+      const [prerelease] = parse(this.version).prerelease
+      this.tag = `${prerelease ?? 'latest'}`
+    }
+
+    if (this.tag !== 'latest' && this.tag !== 'next') {
+      throw new Error(`--tag [${this.tag}] must be 'latest' or 'next'`)
+    }
+  }
+
+  /**
+   * Wipe proxied packages
+   *
+   * @internal
+   */
+  public async wipeProxyDb() {
+    this.log('Wiping previously published packages')
+
+    const verdaccioDb = await readJson(
+      `${ROOTS_PATH}/verdaccio/.verdaccio-db.json`,
+    )
+
+    verdaccioDb.list = []
+
+    await writeJson(
+      `${ROOTS_PATH}/verdaccio/.verdaccio-db.json`,
+      verdaccioDb,
+    )
+
+    await remove(`${ROOTS_PATH}/verdaccio/@roots`)
+  }
+
+  /**
+   * Preflight
+   *
+   * @internal
+   */
+  public async preflight() {
+    if (!process.env.NPM_AUTH_TOKEN) {
+      await this.wipeProxyDb()
+    }
+
+    this.log('Installing')
+    await this.$(`yarn install --immutable`)
+
+    await this.checkTagIsValid()
+  }
+
+  /**
+   * npm version
+   *
+   * @internal
+   */
+  public async bump() {
+    if (this.version) {
+      await this.$(`yarn @bud version ${this.version}`)
+    }
+  }
+
+  /**
+   * Build packages
+   *
+   * @internal
+   */
+  public async make() {
+    await this.$(`yarn @bud build`)
+  }
+
+  /**
+   * Publish release
+   *
+   * @remarks
+   * Publishes release to registry
+   *
+   * @internal
+   */
+  public async publish() {
+    await this.$(`yarn @bud publish --tag ${this.tag}`)
+  }
+}
