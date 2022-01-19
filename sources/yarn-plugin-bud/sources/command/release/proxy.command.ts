59dc31175d0eb61d25b26ea79624dd58ccc1f879
Kelly Mears <developers@tinypixel.dev>
2022-01-18 22:41:25 -0800

    run immutable install on proxy publish

--- sources/yarn-plugin-bud/sources/command/release/proxy.command.ts
+++ sources/yarn-plugin-bud/sources/command/release/proxy.command.ts
@@ -21,7 +21,9 @@ export class ReleaseProxy extends ReleaseNpm {
    *
    * @internal
    */
-  public static paths: CommandClass['paths'] = [[`@bud`, `release`, `proxy`]]
+  public static paths: CommandClass['paths'] = [
+    [`@bud`, `release`, `proxy`],
+  ]
 
   /**
    * Command usage
@@ -53,30 +55,9 @@ export class ReleaseProxy extends ReleaseNpm {
    * @internal
    */
   public async execute() {
-    await this.executeStep(`preflight`)
-    await this.executeStep(`bump`)
     await this.executeStep(`make`)
-    await this.executeStep(`push`)
     await this.executeStep(`prepublish`)
     await this.executeStep(`publish`)
-    await this.executeStep('postpublish')
-  }
-
-  /**
-   * Check token is valid
-   */
-  public async checkHasToken() {}
-
-  /**
-   * npm version
-   *
-   * @remarks
-   * No bump for proxy
-   *
-   * @internal
-   */
-  public async bump() {
-    await this.$(`yarn install --immutable`)
   }
 
   /**
@@ -85,24 +66,15 @@ export class ReleaseProxy extends ReleaseNpm {
    * @internal
    */
   public async make() {
+    await this.$(`yarn install --immutable`)
     await this.$(`yarn @bud build`)
   }
-
-  /**
-   * push
-   *
-   * @remarks
-   * No commit for proxy
-   *
-   * @internal
-   */
-  public async push() {}
 
   /**
    * prepublish
    *
    * @remarks
-   * No prepublish step for proxy
+   * Crudely reset verdaccio db
    *
    * @internal
    */
@@ -113,18 +85,11 @@ export class ReleaseProxy extends ReleaseNpm {
 
     verdaccioDb.list = []
 
-    await writeJson(`${ROOTS_PATH}/verdaccio/.verdaccio-db.json`, verdaccioDb)
+    await writeJson(
+      `${ROOTS_PATH}/verdaccio/.verdaccio-db.json`,
+      verdaccioDb,
+    )
 
     await remove(`${ROOTS_PATH}/verdaccio/@roots`)
   }
-
-  /**
-   * npm postpublish
-   *
-   * @remarks
-   * Reset auth token.
-   *
-   * @internal
-   */
-  public async postpublish() {}
 }
