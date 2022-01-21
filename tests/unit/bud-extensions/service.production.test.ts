48151dd8adad21029dcdce7bcc130a211b119e75
Kelly Mears <developers@tinypixel.dev>
2022-01-21 04:59:33 -0800

    tests/unit/bud-extensions: remove weird hash call

--- tests/unit/bud-extensions/service.production.test.ts
+++ tests/unit/bud-extensions/service.production.test.ts
@@ -8,8 +8,6 @@ describe('Extensions', function () {
   })
 
   it('[production] bud.extensions.repository matches snapshot', () => {
-    bud.hash()
-
     expect(bud.extensions.all()).toMatchSnapshot()
 
     bud.extensions.every((key, controller) => {
