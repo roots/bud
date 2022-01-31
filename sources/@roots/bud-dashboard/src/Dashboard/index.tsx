2ef2085d040ec6edf88cc5c341d94e9d4e346256
Kelly Mears <developers@tinypixel.dev>
2022-01-31 02:38:43 -0800

    fix: conditial dashboard imports

--- sources/@roots/bud-dashboard/src/Dashboard/index.tsx
+++ sources/@roots/bud-dashboard/src/Dashboard/index.tsx
@@ -1,7 +1,6 @@
 import {Dashboard as Contract, Framework} from '@roots/bud-framework'
 import {Service} from '@roots/bud-framework'
 import {bind, chalk, once} from '@roots/bud-support'
-import {render} from 'ink'
 import React from 'react'
 
 /**
@@ -16,11 +15,10 @@ export class Dashboard extends Service implements Contract {
    */
   @bind
   public async bootstrap(): Promise<void> {
-    this.log('log', chalk.green('initializing dashboard'))
-
-    if (this.app.store.is('features.dashboard', true)) {
-      await this.run()
-    }
+    this.app.hooks.async('event.compiler.after', async app => {
+      app.store.is('features.dashboard', true) && (await this.run())
+      return app
+    })
   }
 
   /**
@@ -34,14 +32,10 @@ export class Dashboard extends Service implements Contract {
   @once
   public async run(): Promise<Framework> {
     const {Dashboard} = await import('../components')
+    const {render} = await import('ink')
 
-    this.app.hooks.async('event.compiler.after', async app => {
-      this.log('success', {message: 'rendering dashboard'})
-      process.stdout.write('\n')
-      render(<Dashboard tap={() => this.app.root} />)
-
-      return app
-    })
+    this.log('info', {message: chalk.blue(`rendering dashboard\n`)})
+    render(<Dashboard tap={() => this.app.root} />)
 
     return this.app
   }
