2308f530de24badb8b69331a848e87f9aea249b4
Kelly Mears <developers@tinypixel.dev>
2022-01-31 08:05:10 -0800

    fix: @roots/bud-eslint loader cache (#1068)

--- sources/@roots/bud-eslint/src/eslint.options.ts
+++ sources/@roots/bud-eslint/src/eslint.options.ts
@@ -1,15 +1,22 @@
 import type {Framework} from '@roots/bud-framework'
 import type {Options} from 'eslint-webpack-plugin'
+import {cpus} from 'os'
 
 export interface options {
   (app: Framework): Options
 }
 
-export const options: options = ({path}) => ({
+export const options: options = ({store, path}) => ({
   extensions: ['js', 'jsx', 'ts', 'tsx', 'vue'],
-  cache: true,
+  cache: store.isTrue('features.cache'),
   cacheLocation: path('storage', 'cache', 'eslint.json'),
+  cacheStrategy: 'content',
   context: path('src'),
   cwd: path('project'),
+  emitError: true,
+  emitWarning: true,
   failOnError: true,
+  eslintPath: require.resolve('eslint'),
+  resolvePluginsRelativeTo: path('project'),
+  threads: cpus.length / 2,
 })
