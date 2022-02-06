09abc07078f282bfbb43edb8e850d04cab2405bd
Kelly Mears <developers@tinypixel.dev>
2022-02-06 09:19:04 -0800

    style: duplicative copyright

--- sources/@roots/bud-extensions/src/index.ts
+++ sources/@roots/bud-extensions/src/index.ts
@@ -1,11 +1,8 @@
 // Copyright (c) Roots Foundation, LLC. All rights reserved.
 // Licensed under the MIT license.
 
-// Copyright (c) Roots Foundation, LLC. All rights reserved.
-// Licensed under the MIT license.
-
 /**
- * The {@link @roots/bud-extensions#} package implements the {@link @roots/bud-framework#Extensions | Extensions interface}
+ * The `@roots/bud-extensions` package implements the {@link @roots/bud-framework#Extensions} interface.
  *
  * @see https://roots.io/bud
  * @see https://github.com/roots/bud
