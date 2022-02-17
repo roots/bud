b97f75c89e2527217ac9ecf51924e423362bd38f
Kelly Mears <developers@tinypixel.dev>
2022-02-17 05:21:58 -0800

    improve: bud.entry

--- sources/@roots/bud-api/src/api/methods/entry/index.ts
+++ sources/@roots/bud-api/src/api/methods/entry/index.ts
@@ -1,5 +1,6 @@
 import type {Framework} from '@roots/bud-framework'
 import type {EntryObject} from '@roots/bud-framework/types/entry'
+import {isArray} from 'lodash'
 
 import {globby, isString} from './entry.dependencies'
 
@@ -24,42 +25,32 @@ const isGlobular = (str: string) =>
   ['*', '{', '}'].filter(c => str.includes(c)).length > 0
 
 export const entry: entry = async function (
-  ...entrypoint: Input
+  ...useInput: Input
 ): Promise<Framework> {
   const ctx = this as Framework
   const glob: typeof globAssets = globAssets.bind(this)
 
-  const userInput = isString(entrypoint[0])
-    ? {
-        [entrypoint[0]]: {
-          import: isString(entrypoint[1])
-            ? [entrypoint[1]]
-            : entrypoint[1],
-          dependOn: [],
-        },
-      }
-    : entrypoint[0]
+  const normalizedInput = isString(useInput[0])
+    ? {[useInput[0]]: useInput[1]}
+    : useInput[0]
 
   ctx.hooks.async('build.entry', async all => {
     await Promise.all(
-      Object.entries(userInput).map(async ([name, input]) => {
-        input.import = input.import ?? input
+      Object.entries(normalizedInput).map(async ([name, entry]) => {
+        const normalizedImports = isArray(entry.imports ?? entry)
+          ? entry.imports ?? entry
+          : [entry.imports ?? entry]
 
-        input.import = (
+        const value = (
           await Promise.all(
-            input.import.map(async (request: string) =>
+            normalizedImports.map(async (request: string) =>
               isGlobular(request) ? await glob(request) : request,
             ),
           )
         ).flat()
 
         Object.assign(all, {
-          [name]: input.dependOn?.length
-            ? {
-                import: input.import.flat(),
-                dependOn: input.dependOn.flat(),
-              }
-            : {import: input.import.flat()},
+          [name]: {import: value, ...(entry.dependOn ?? {})},
         })
       }),
     )
@@ -73,19 +64,9 @@ export const entry: entry = async function (
 export async function globAssets(search: string): Promise<Array<string>> {
   const globDir = this.path('src')
 
-  this.info({
-    message: 'glob search',
-    suffix: JSON.stringify(search),
-  })
-
   try {
     const results = await globby.globby(search, {
       cwd: this.path('src'),
-    })
-
-    this.info({
-      message: 'glob results',
-      suffix: JSON.stringify(results),
     })
 
     if (!results.length) {
