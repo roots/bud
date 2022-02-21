ee98ae090af02db58413d4ce65e1a0ca1421787f
Kelly Mears <developers@tinypixel.dev>
2022-02-21 02:40:28 -0800

    fix: bud init/bud install deprecation notice

---
+++ sources/@roots/bud/src/cli/commands/install.ts
@@ -0,0 +1,19 @@
+import {chalk} from '@roots/bud-support'
+
+import {BuildCommand} from './build.js'
+
+/**
+ * @public
+ */
+export class InstallCommand extends BuildCommand {
+  public static paths = [[`install`], [`init`]]
+
+  public async execute() {
+    process.stdout.write(
+      chalk.red(`\nbud install / bud init is deprecated\n`),
+    )
+    process.stdout.write(
+      `This command was deprecated in 5.3.0. In the future this command will throw an error.`,
+    )
+  }
+}
