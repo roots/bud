c090c594b03e234ec8779efcc4093bda76860e1e
Kelly Mears <developers@tinypixel.dev>
2022-02-23 05:12:45 -0800

    typings: improve extension typings

--- sources/@roots/bud-babel/src/babel.extension.ts
+++ sources/@roots/bud-babel/src/babel.extension.ts
@@ -1,67 +1,100 @@
 import {Item, Loader} from '@roots/bud-build'
-import {Framework} from '@roots/bud-framework'
+import {Extension, Framework} from '@roots/bud-framework'
 
 import {Config} from './babel.config'
 import {DEFAULT_PLUGINS, DEFAULT_PRESETS} from './babel.constants'
 
 /**
- * Adds Babel transpiler support to Framework projects
+ * Babel options
  *
  * @public
  */
-export const name = '@roots/bud-babel'
+export interface Options {
+  cacheDirectory: string
+  env: any
+  root: string
+  presets?: any
+  plugins?: any
+}
 
 /**
- * Exposes app.babel configuration utility
+ * Adds Babel transpiler support
  *
  * @public
  */
-export const mixin = async app => ({
+export interface BabelExtension extends Extension.Module<Options> {
+  name: '@roots/bud-babel'
+  mixin: (app: Framework) => Promise<{babel: [typeof Config, Framework]}>
+  options: (app: Framework) => Options
+  register: (app: Framework) => Promise<void>
+}
+
+/**
+ * Extension name
+ *
+ * @public
+ */
+export const name: BabelExtension['name'] = '@roots/bud-babel'
+
+/**
+ * Bud mixins
+ *
+ * @remarks
+ * Registers `bud.babel` configuration class
+ *
+ * @public
+ */
+export const mixin: BabelExtension['mixin'] = async app => ({
   babel: [Config, app],
 })
 
 /**
- * Extension register event
+ * Extension options
  *
  * @public
  */
-export const register = async (app: Framework) => {
-  app.build.loaders.babel = new Loader(() =>
-    require.resolve('babel-loader'),
-  )
-
-  app.build.items.babel = new Item({
-    loader: ({build}) => build.loaders.babel,
-    options: app => {
-      const options: {
-        cacheDirectory: string
-        env: any
-        root: string
-        presets?: any
-        plugins?: any
-      } = {
-        cacheDirectory: app.path('storage', 'cache', 'babel'),
-        env: {
-          development: {
-            compact: false,
-          },
-        },
-        root: app.path('src'),
-      }
-
-      if (app.babel?.presets) {
-        options.presets = Object.values(app.babel.presets)
-      }
-
-      if (app.babel?.plugins) {
-        options.plugins = Object.values(app.babel.plugins)
-      }
-
-      return options
+export const options: BabelExtension['options'] = app => ({
+  cacheDirectory: app.path('storage', 'cache', 'babel'),
+  env: {
+    development: {
+      compact: false,
     },
+  },
+  root: app.path('src'),
+  ...(app.babel.presets
+    ? {presets: Object.values(app.babel.presets)}
+    : {}),
+  ...(app.babel.plugins
+    ? {plugins: Object.values(app.babel.plugins)}
+    : {}),
+})
+
+/**
+ * Extension registration
+ *
+ * @public
+ */
+export const register: BabelExtension['register'] = async app => {
+  /**
+   * Babel loader
+   */
+  app.build.loaders.babel = new Loader(require.resolve('babel-loader'))
+
+  /**
+   * Babel ruleset item
+   */
+  app.build.items.babel = new Item({
+    loader: app => app.build.loaders.babel,
+    options: app => app.extensions.get('@roots/bud-babel').options.all(),
   })
 
-  app.build.rules.js.setUse([app.build.items.babel])
+  /**
+   * Babel ruleset rule
+   */
+  app.build.rules.js.setUse(app => [app.build.items.babel])
 
+  /**
+   * Default babel presets and plugins
+   */
   app.babel.setPresets(DEFAULT_PRESETS).setPlugins(DEFAULT_PLUGINS)
 }
