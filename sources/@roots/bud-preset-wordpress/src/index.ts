7a4942030b7dd1040ab1b14557b81cdd81c8777f
Kelly Mears <developers@tinypixel.dev>
2022-02-24 06:44:23 -0800

    export theme.json types from wordpress-preset

--- sources/@roots/bud-preset-wordpress/src/index.ts
+++ sources/@roots/bud-preset-wordpress/src/index.ts
@@ -7,42 +7,6 @@
  * @see https://roots.io/bud
  * @see https://github.com/roots/bud
  *
- * @remarks
- * - 💁 Composable - Build boss web applications with a modular, configurable build system
- *
- * - 💪 Modern - Modern framework that scales from a single file to thousands of lines of code
- *
- * - 🌱 Easy - Low bundle size and fast build times
- *
- * @remarks
- * This preset is a wrapper for the following presets:
- *
- * - {@link @roots/bud-preset-recommend# | @roots/bud-preset-recommend}
- *
- * - {@link @roots/bud-react# | @roots/bud-react}
- *
- * - {@link @roots/bud-wordpress-dependencies# | @roots/bud-wordpress-dependencies}
- *
- * - {@link @roots/bud-wordpress-externals# | @roots/bud-wordpress-externals}
- *
- * - {@link @roots/bud-wordpress-manifests# | @roots/bud-wordpress-manifests}
- *
- * @example
- * ```js
- * const wp = require('@roots/bud-preset-wordpress')
- *
- * module.exports = (app: Framework) => {
- *   app.use(wp)
- * }
- * ```
- *
- * @remarks
- * - 💁 Composable - Build exceptional applications with a modular, configurable build system
- *
- * - 💪 Modern - Modern framework written in TypeScript with an expressive API
- *
- * - 🌱 Easy - Low bundle size and fast build times
- *
  * @packageDocumentation
  */
 
@@ -71,3 +35,5 @@ type BudWordPressPreset = Extension.Module
 
 export const name: BudWordPressPreset['name'] =
   '@roots/bud-preset-wordpress'
+
+export * as ThemeJSON from './theme'
