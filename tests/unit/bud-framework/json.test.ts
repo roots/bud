d3eda2a31355450b513742d132f3cb39745aaa80
Kelly Mears <developers@tinypixel.dev>
2022-04-05 09:34:09 -0700

    :test_tube: test(none): @roots/bud-framework close, json, yml

---
+++ tests/unit/bud-framework/json.test.ts
@@ -0,0 +1,24 @@
+import {paths} from '@repo/constants'
+import {read,write} from '@roots/bud-framework/src/Framework/methods/json5'
+import {join} from 'node:path'
+
+describe('bud.json', () => {
+  describe('read', () => {
+    it('read is a fn', async () => {
+      expect(read).toBeInstanceOf(Function)
+    })
+    it('reads json', async () => {
+      const manifest = await read(join(paths.root, 'package.json'))
+      expect(manifest.name).toBe('bud')
+    })
+  })
+
+
+  describe('write', () => {
+    it('is a fn', async () => {
+      expect(write).toBeInstanceOf(Function)
+    })
+
+    test.todo('writes json')
+  })
+})
---
+++ tests/unit/bud-framework/yaml.test.ts
@@ -0,0 +1,24 @@
+import {paths} from '@repo/constants'
+import {read,write} from '@roots/bud-framework/src/Framework/methods/yaml'
+import {join} from 'node:path'
+
+describe('bud.yml', () => {
+  describe('read', () => {
+    it('read is a fn', async () => {
+      expect(read).toBeInstanceOf(Function)
+    })
+    it('reads yml', async () => {
+      const manifest = await read(join(paths.root, '.yarnrc.yml'))
+      expect(manifest.enableTelemetry).toBe(false)
+    })
+  })
+
+
+  describe('write', () => {
+    it('is a fn', async () => {
+      expect(write).toBeInstanceOf(Function)
+    })
+
+    test.todo('writes yml')
+  })
+})
