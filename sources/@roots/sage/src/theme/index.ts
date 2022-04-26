9cb4747c1fc3b207314730ef44c2468039d34ae2
Kelly Mears <developers@tinypixel.dev>
2022-04-26 05:28:32 -0700

    📦 deps(none): lockfix

--- sources/@roots/sage/src/theme/index.ts
+++ sources/@roots/sage/src/theme/index.ts
@@ -1,4 +1,4 @@
-import type {JSONSchemaForWordPressBlockThemeGlobalSettingsAndStyles as WPThemeJson} from '@roots/bud-preset-wordpress/types/theme'
+import type {GlobalSettingsAndStyles as WPThemeJson} from '@roots/bud-preset-wordpress/types/theme'
 
 import * as Extension from './extension'
 import * as Plugin from './plugin'
