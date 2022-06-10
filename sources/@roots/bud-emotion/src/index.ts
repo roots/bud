d021b1b166ceacb018acdb777269377771469c9a
Kelly Mears <developers@tinypixel.dev>
2022-06-10 14:41:17 -0700

    🛼 improve(patch): make @roots/bud-emotion disable-able (#1461)
    * 🛼 improve(patch): make @roots/bud-emotion disable-able

--- sources/@roots/bud-emotion/src/index.ts
+++ sources/@roots/bud-emotion/src/index.ts
@@ -2,15 +2,13 @@
 // Licensed under the MIT license.
 
 /**
- * The `@roots/bud-emotion` package adds emotioncss support to {@link @roots/bud-framework# | the Bud}
+ * Adds emotioncss support to `@roots/bud`
  *
  * @see https://bud.js.org
  * @see https://github.com/roots/bud
  *
  * @packageDocumentation
  */
-
-import './env.js'
 
 import BudEmotion from './extension.js'
 export default BudEmotion
