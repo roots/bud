0f44e0b3a49de4601351e6652c4a151bdf900b95
Kelly Mears <developers@tinypixel.dev>
2022-06-20 05:48:16 -0700

    🧪 fix(none): ts test

--- sources/@roots/bud/bin/ts-bud.mjs
+++ sources/@roots/bud/bin/ts-bud.mjs
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S ts-node --esm --transpileOnly --skipProject --cwdMode
+#!/usr/bin/env -S ts-node --esm --transpileOnly --cwdMode
 
 (async () => {
   await import('../lib/cli/index.js');
