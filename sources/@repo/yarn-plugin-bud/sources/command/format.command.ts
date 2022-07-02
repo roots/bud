2e59b5846581c4aa4ea6359cebd0d81912c98012
Kelly Mears <developers@tinypixel.dev>
2022-07-02 12:42:22 -0700

    🔧 dev(none): fix ci

--- sources/@repo/yarn-plugin-bud/sources/command/format.command.ts
+++ sources/@repo/yarn-plugin-bud/sources/command/format.command.ts
@@ -55,14 +55,6 @@ export class Format extends Command {
       `yarn prettier ./sources/@roots/*/src/**/* ${
         this.fix ? '--write' : ''
       } --config ./config/prettier.config.cjs --ignore-unknown --no-error-on-unmatched-pattern`,
-
-      `yarn prettier ./sources/@roots/*/lib/**/* ${
-        this.fix ? '--write' : ''
-      } --config ./config/prettier.config.cjs --write --ignore-unknown --no-error-on-unmatched-pattern`,
-
-      `yarn prettier ./sources/@roots/*/types/**/*.d.ts ${
-        this.fix ? '--write' : ''
-      } --config ./config/prettier.config.cjs --write --ignore-unknown --no-error-on-unmatched-pattern`,
     )
   }
 }
