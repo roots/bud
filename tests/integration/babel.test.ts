c0475fdc51966a843edb903ea78a67d49a9d1757
Kelly Mears <developers@tinypixel.dev>
2022-07-04 14:27:36 -0700

    🔧 fix(none): remove intentionally broken test

--- tests/integration/babel.test.ts
+++ tests/integration/babel.test.ts
@@ -27,12 +27,6 @@ const run = pacman => () => {
       expect(project.manifest).toMatchSnapshot()
     })
   })
-
-  describe('qanon', () => {
-    it('makes sense', () => {
-      expect(1 + 1).toEqual(3)
-    })
-  })
 }
 
 describe('babel', () => {
