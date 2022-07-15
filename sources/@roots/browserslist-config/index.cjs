5eea047b6bc5f9d29ca1ebe729c6fbd6e26b654c
Kelly Mears <developers@tinypixel.dev>
2022-07-14 21:32:20 -0700

    🩹 fix(patch): browserslist-config compatibility

--- sources/@roots/browserslist-config/index.cjs
+++ sources/@roots/browserslist-config/index.cjs
@@ -1,20 +1,10 @@
 /**
  * Browserslist default
  */
-module.exports = {
-  production: [
-    '> 1%',
-    'last 2 chrome versions',
-    'last 2 firefox versions',
-    'last 2 safari versions',
-    'last 2 ios versions',
-    'last 2 edge versions',
-    'last 2 opera versions',
-    'last 1 android versions',
-  ],
-  development: [
-    'last 1 chrome version',
-    'last 1 firefox version',
-    'last 1 safari version',
-  ],
-}
+module.exports = [
+  '> 0.5%',
+  'last 2 versions',
+  'not dead',
+  'not op_mini all',
+  'supports es6-module-dynamic-import',
+]
