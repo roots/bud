b7204f4869bc1864f4e86c553a42435f00304055
Kelly Mears <developers@tinypixel.dev>
2022-07-28 02:09:29 -0700

    ✨ improve(patch): improve bootstrap

--- sources/@roots/bud-framework/src/lifecycle/index.ts
+++ sources/@roots/bud-framework/src/lifecycle/index.ts
@@ -1,19 +1,11 @@
-import {isFunction, omit} from 'lodash-es'
+import {isFunction} from 'lodash-es'
 
-import type {Bud, Config, Services} from '../index.js'
-import {Logger} from '../logger/index.js'
-import * as methods from '../methods/index.js'
-import {Module} from '../module.js'
-import * as Process from '../process.js'
-import type {Service} from '../service.js'
-import {
-  DEVELOPMENT_SERVICES,
-  LIFECYCLE_EVENTS,
-  PARENT_SERVICES,
-} from './constants.js'
+import type {Bud, Config} from '../index.js'
+import * as bootstrap from './bootstrap.js'
+import {LIFECYCLE_EVENTS} from './constants.js'
 
 /**
- * Bootstrap interface
+ * Lifecycle interface
  *
  * @public
  */
@@ -21,12 +13,29 @@ export interface lifecycle {
   (this: Bud, options: Config.Options): Promise<Bud>
 }
 
+const makeServiceEventMapper =
+  (app: Bud, event: string) =>
+  async ([ident, fn]) => {
+    try {
+      await fn(app)
+      app.success(event, ident)
+    } catch (err) {
+      app.warn(`Error executing`, event, `for`, ident).error(err)
+    }
+  }
+
+const makeServiceToTuple = (event: string) => service =>
+  [service, service[event]]
+
+const makeServiceEventFilter = (event: string) => service =>
+  isFunction(service[event])
+
 /**
- * Initializes and binds {@link Bud.services}
+ * Initializes and binds service lifecycle methods
  *
  * @example
  * ```js
- * new BudImplementation(...constructorParams).bootstrap()
+ * new BudImplementation(...constructorParams).lifecycle()
  * ```
  *
  * @param this - {@link Bud}
@@ -38,68 +47,17 @@ export async function lifecycle(
   this: Bud,
   options: Config.Options,
 ): Promise<Bud> {
-  this.children = {}
-  this.options = omit({...options}, 'context')
-  this.context = {...options.context, dir: options.dir}
-
-  process.env.NODE_ENV = options.mode
-
-  Object.entries(methods).map(([key, method]) => {
-    this[key] = method.bind(this)
-  })
-
-  if (!this.isRoot) Process.initialize(this)
-
-  this.logger = new Logger(this)
-  this.module = new Module(this)
-
-  this.services = Object.entries({...options.services})
-    .filter(
-      ([name]) =>
-        this.isDevelopment || !DEVELOPMENT_SERVICES.includes(name),
-    )
-    .filter(([name]) => this.isRoot || !PARENT_SERVICES.includes(name))
-    .map(
-      ([name, Service]): [keyof Services.Registry & string, Service] => {
-        this.log('initializing', name)
-
-        this[name] = new Service(this)
-
-        return [name, this[name]]
-      },
-    )
-    .reduce((a, [k, v]): Services.Registry => ({...a, [k]: v}), {})
+  bootstrap.execute(this, options)
 
   await LIFECYCLE_EVENTS.reduce(async (promised, event) => {
-    await promised
+    if (promised) await promised
 
     await Promise.all(
-      Object.keys(this.services)
-        .filter(service => isFunction(this[service][event]))
-        .map(service => [
-          service,
-          this[service][event].bind(this[service]),
-        ])
-        .map(async ([service, serviceLifecycleMethod]) => {
-          try {
-            await serviceLifecycleMethod(this)
-            this.success({
-              message: event,
-              suffix: service.constructor.name.toLowerCase(),
-            })
-          } catch (err) {
-            this.error(
-              `Error executing`,
-              event,
-              `for service`,
-              service.constructor.name.toLowerCase(),
-              `\n`,
-              err,
-            )
-          }
-        }),
+      Object.values(this.services)
+        .filter(makeServiceEventFilter(event))
+        .map(makeServiceToTuple(event))
+        .map(makeServiceEventMapper(this, event)),
     )
-
     return Promise.resolve()
   }, Promise.resolve())
 
