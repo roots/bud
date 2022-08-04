66453fb1337ba2862091101cdd489d68cdab4f56
Kelly Mears <developers@tinypixel.dev>
2022-08-04 15:37:42 -0700

    🧪 test(none): improve tests structure (#1623)
    This test setup lets us run coverage again without dealing with monorepo tjs resolution issues.
    
    - switches to v8 from babel
    - starts moving unit tests out into the packages (easier to resolve, more accessible as a reference)
    
    refers:
    
    - none
    
    ## Type of change
    
    **NONE: internal change**
    
    
    
    This PR includes breaking changes to the following core packages:
    
    - none
    
    This PR includes breaking changes to the follow extensions:
    
    - none
    
    ## Dependencies
    
    ### Adds
    
    - none
    
    ### Removes
    
    - none

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,54 +1,93 @@
+import {paths} from '@repo/constants'
+
+/**
+ * Base jest configuration
+ */
+const base = {
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['sources/**/*{ts,tsx}'],
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  moduleDirectories: ['node_modules'],
+  moduleNameMapper: {
+    /**
+     * Jest doesn't understand .js extension with es modules
+     */
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    /**
+     * Jest doesn't understand # in import maps
+     */
+    '#ansi-styles':
+      '<rootDir>/node_modules/chalk/source/vendor/ansi-styles/index.js',
+    '#supports-color':
+      '<rootDir>/node_modules/chalk/source/vendor/supports-color/index.js',
+  },
+  modulePathIgnorePatterns: ['<rootDir>/.yarn', '<rootDir>/storage'],
+  rootDir: paths.root,
+  testEnvironment: 'node',
+  testPathIgnorePatterns: [
+    '<rootDir>/build/',
+    '<rootDir>/node_modules/',
+    '<rootDir>/tests/__mocks__',
+    '<rootDir>/storage/',
+  ],
+  testTimeout: 240000,
+  slowTestThreshold: 240000,
+  transform: {
+    '^.+\\.(c|m)?(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            jsx: true,
+            dynamicImport: true,
+            optionalChaining: true,
+            decorators: true,
+            importMeta: true,
+          },
+          target: 'es2021',
+        },
+        sourceMaps: true,
+      },
+    ],
+  },
+  verbose: true,
+}
+
 /**
  * Jest configuration
  */
-export default async function config() {
-  return {
-    extensionsToTreatAsEsm: ['.ts', '.tsx'],
-    moduleDirectories: ['node_modules'],
-    /* @prettier-ignore */
-    moduleNameMapper: {
-      /**
-       * Jest doesn't understand ts with es modules
-       */
-      '^(\\.{1,2}/.*)\\.js$': '$1',
-      /**
-       * Jest doesn't understand # in import maps
-       */
-      '#ansi-styles':
-        '<rootDir>/node_modules/chalk/source/vendor/ansi-styles/index.js',
-      '#supports-color':
-        '<rootDir>/node_modules/chalk/source/vendor/supports-color/index.js',
+export default async () => ({
+  ...base,
+  projects: [
+    {
+      ...base,
+      displayName: 'e2e',
+      reporters: ['default'],
+      testMatch: ['**/tests/e2e/**/*.test.ts'],
     },
-    modulePathIgnorePatterns: ['<rootDir>/.yarn', '<rootDir>/storage'],
-    rootDir: '../',
-    slowTestThreshold: 30000,
-    testEnvironment: 'node',
-    testMatch: [`<rootDir>/tests/**/*.test.ts`],
-    testPathIgnorePatterns: [
-      '<rootDir>/build/',
-      '<rootDir>/node_modules/',
-      '<rootDir>/tests/__mocks__',
-      '<rootDir>/cache/verdaccio',
-    ],
-    testTimeout: 60000,
-    transform: {
-      '^.+\\.(c|m)?(t|j)sx?$': [
-        '@swc/jest',
-        {
-          jsc: {
-            parser: {
-              syntax: 'typescript',
-              jsx: true,
-              dynamicImport: true,
-              optionalChaining: true,
-              decorators: true,
-              importMeta: true,
-            },
-            target: 'es2021',
-          },
-          sourceMaps: true,
-        },
+    {
+      ...base,
+      displayName: 'integration',
+      reporters: ['default'],
+      testMatch: ['**/tests/integration/**/*.test.ts'],
+    },
+    {
+      ...base,
+      displayName: 'unit',
+      collectCoverage: true,
+      coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '/vendor/',
+        '/lib/',
+        '/@repo/',
+        '/tests/',
+      ],
+      reporters: ['default', 'github-actions'],
+      testMatch: [
+        '**/sources/@roots/*/src/**/*.test.ts',
+        '**/tests/unit/**/*.test.ts',
       ],
     },
-  }
-}
+  ],
+})
---
+++ sources/@roots/bud-api/src/methods/assets/assets.test.ts
@@ -0,0 +1,92 @@
+import {beforeEach, describe, expect, it} from '@jest/globals'
+import {Bud, factory} from '@repo/test-kit/bud'
+
+import {assets} from './assets.method.js'
+
+describe('bud.assets', function () {
+  let bud: Bud
+  let instance: typeof assets
+
+  beforeEach(async () => {
+    bud = await factory()
+    instance = assets.bind(bud)
+  })
+
+  it('should be a function', () => {
+    expect(assets).toBeInstanceOf(Function)
+  })
+
+  it('should have copy-webpack-plugin available', () => {
+    expect(bud.extensions.has('copy-webpack-plugin')).toBeTruthy()
+  })
+
+  it('should add job when passed an array of strings', async () => {
+    await instance(['images'])
+
+    expect(
+      bud.extensions.get('copy-webpack-plugin').getOption('patterns'),
+    ).toMatchSnapshot([
+      {
+        from: expect.stringContaining('images'),
+        to: expect.stringContaining('dist/images/[path][name][ext]'),
+        context: expect.stringContaining('src'),
+        toType: 'template',
+        noErrorOnMissing: true,
+      },
+    ])
+  })
+
+  it('should add jobs when passed an array of tuples', async () => {
+    await instance([
+      [bud.path('@src/images'), bud.path('@dist/images')],
+      [
+        bud.path('@src/fonts/font.woff'),
+        bud.path('@dist/fonts/font.woff'),
+      ],
+    ])
+
+    const patterns = bud.extensions
+      .get('copy-webpack-plugin')
+      .getOption('patterns')
+      ?.slice(0, 2)
+
+    expect(patterns).toMatchSnapshot([
+      {
+        from: expect.stringMatching(/tests\/util\/project\/src\/images$/),
+        to: expect.stringMatching(/tests\/util\/project\/dist\/images$/),
+        context: expect.stringContaining('src'),
+        toType: 'template',
+        noErrorOnMissing: true,
+      },
+      {
+        from: expect.stringMatching(
+          /tests\/util\/project\/src\/fonts\/font.woff$/,
+        ),
+        to: expect.stringMatching(
+          /tests\/util\/project\/dist\/fonts\/font.woff$/,
+        ),
+        context: expect.stringContaining('src'),
+        toType: 'template',
+        noErrorOnMissing: true,
+      },
+    ])
+  })
+
+  it('should add jobs when passed an object', async () => {
+    const input = {
+      from: bud.path('@src/images'),
+      to: bud.path('@dist/images'),
+    }
+
+    await instance(input)
+
+    const patterns = bud.extensions
+      .get('copy-webpack-plugin')
+      .getOption('patterns')
+
+    expect(patterns?.pop()).toMatchSnapshot({
+      from: expect.stringMatching(/tests\/util\/project\/src\/images$/),
+      to: expect.stringMatching(/tests\/util\/project\/dist\/images$/),
+    })
+  })
+})
