f2fdd702d41d1140c34ce00bc70c99e7bd0fb9d7
Kelly Mears <developers@tinypixel.dev>
2022-08-08 07:50:51 -0700

    mergefix

--- sources/@roots/bud-server/src/seed.ts
+++ sources/@roots/bud-server/src/seed.ts
@@ -23,17 +23,6 @@ export const seed = (app: Bud) => {
     .hooks.on(`dev.middleware.dev.options.publicPath`, () =>
       app.hooks.filter('build.output.publicPath'),
     )
-    .hooks.on(`dev.middleware.dev.options.stats`, 'none')
-    .hooks.on(`dev.middleware.dev.options.writeToDisk`, true)
-
-    .hooks.on(`dev.middleware.hot.options`, () => ({
-      path: app.hooks.filter('dev.middleware.hot.options.path'),
-      log: app.hooks.filter('dev.middleware.hot.options.log'),
-      heartbeat: app.hooks.filter('dev.middleware.hot.options.heartbeat'),
-    }))
-    .hooks.on(`dev.middleware.hot.options.path`, () => `/__bud/hmr`)
-    .hooks.on(`dev.middleware.hot.options.log`, false)
-    .hooks.on(`dev.middleware.hot.options.heartbeat`, 2000)
     .hooks.on(`dev.client.scripts`, clientScripts.callback)
     .hooks.on(`dev.watch.files`, new Set([]))
     .hooks.on(`dev.watch.options`, {})
