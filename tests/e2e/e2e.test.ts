732ef537491d13f27cf5a67e3365123af1dd4da5
Kelly Mears <developers@tinypixel.dev>
2022-08-12 14:14:25 -0700

    fix: test ci

--- tests/e2e/e2e.test.ts
+++ tests/e2e/e2e.test.ts
@@ -3,5 +3,6 @@ import {describe} from '@jest/globals'
 import * as css from './e2e.css'
 import * as js from './e2e.script'
 
+jest.setTimeout(3 * 60 * 1000)
 describe('html output of examples/babel', css.test)
 describe('html output of examples/basic', js.test)
