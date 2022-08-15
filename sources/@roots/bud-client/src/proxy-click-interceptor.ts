b09540e892401551ee97a179dba31b6f98f6542c
Kelly Mears <developers@tinypixel.dev>
2022-08-15 11:38:48 -0700

    🩹 fix (patch): proxy click interceptor (#1638)
    fixes new issue with proxy interception
    
    refers:
    
    - #1636 
    - #1594 
    
    ## Type of change
    
    **PATCH: backwards compatible change**
    
    
    
    This PR includes breaking changes to the following core packages:
    
    - none
    
    This PR includes breaking changes to the follow extensions:
    
    - none
    
    ## Dependencies
    
    ### Adds
    
    - none
    
    ### Removes
    
    - none

--- sources/@roots/bud-client/src/proxy-click-interceptor.ts
+++ sources/@roots/bud-client/src/proxy-click-interceptor.ts
@@ -1,52 +1,19 @@
 /* eslint-disable no-console */
 /* global __resourceQuery */
 
-type Target = HTMLAnchorElement | HTMLLinkElement | HTMLFormElement
-type ElementTuple = [HTMLCollectionOf<Target>, any]
+import intercept from './intercept'
 
-const main = async (proxy: string = null) => {
-  proxy = proxy ?? new URLSearchParams(__resourceQuery).get('proxy')
+window.requestAnimationFrame(async function ready() {
+  if (!__resourceQuery) return
+  const params = new URLSearchParams(__resourceQuery)
 
-  try {
-    setInterval(
-      () =>
-        [
-          [document.getElementsByTagName('a'), 'href'],
-          [document.getElementsByTagName('link'), 'href'],
-        ]
-          .map(
-            ([elements, attribute]: ElementTuple): [
-              Array<Target>,
-              any,
-            ] => [Array.from(elements), attribute],
-          )
-          .forEach(([elements, attribute]: [Array<Target>, any]) =>
-            elements
-              .filter(el => el.hasAttribute(attribute))
-              .filter(el => !el.hasAttribute('__bud_processed'))
-              .filter(el => el.getAttribute(attribute).startsWith(proxy))
-              .map(el => {
-                const value = el.getAttribute(attribute)
-                console.info(
-                  `replacing ${attribute} on ${el.tagName} with value of ${value}`,
-                )
-                el.setAttribute(attribute, value.replace(proxy, '/'))
-                el.toggleAttribute('__bud_processed')
-              }),
-          ),
-      1000,
-    )
-  } catch (err) {
-    return console.error(
-      `There was a problem replacing hrefs in the proxied response. Exiting script early.`,
-      err,
-    )
-  }
-}
+  if (!params) return
+  const href = params.get('href')
 
-;(async () =>
-  window.requestAnimationFrame(async function ready() {
-    return document.body
-      ? await main()
-      : window.requestAnimationFrame(ready)
-  }))()
+  if (!href) return
+  const origin = decodeURI(href)
+
+  return document.body
+    ? intercept(origin)
+    : window.requestAnimationFrame(ready)
+})
