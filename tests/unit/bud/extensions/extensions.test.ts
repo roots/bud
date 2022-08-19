f5d221d767b5351159067b9bb2703e4ba4a9d12c
Kelly Mears <developers@tinypixel.dev>
2022-08-19 05:40:36 -0700

    fix: failing test

--- tests/unit/bud/extensions/extensions.test.ts
+++
@@ -1,9 +0,0 @@
-import extensions from '@roots/bud/extensions'
-
-describe(`@roots/bud`, () => {
-  describe(`extensions`, () => {
-    it(`is a function\``, () => {
-      expect(extensions).toBeDefined()
-    })
-  })
-})
