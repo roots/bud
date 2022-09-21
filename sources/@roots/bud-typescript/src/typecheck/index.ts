40e7a1bc9d7818699c6fd18f85d605047b818ede
Kelly Mears <developers@tinypixel.dev>
2022-09-20 21:30:23 -0700

    https://github.com/microsoft/TypeScript/issues/50717

--- sources/@roots/bud-typescript/src/typecheck/index.ts
+++ sources/@roots/bud-typescript/src/typecheck/index.ts
@@ -29,7 +29,7 @@ export default class BudTypeCheckPlugin extends Extension<
   /**
    * Enable typechecking
    *
-   * @override {@link Extension.enable}
+   * @override
    * @decorator `@bind`
    */
   @bind public async enable(state: boolean = true) {
