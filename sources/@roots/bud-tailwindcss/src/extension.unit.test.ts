bef11fbcc3f78d85804b38259e8b02b7da48c5c0
Kelly Mears <developers@tinypixel.dev>
2022-09-25 11:15:09 -0700

    🧪 test(none): @roots/bud-tailwindcss (#1705)
    increased coverage
    
    refers:
    
    - none
    
    ## Type of change
    
    **PATCH: backwards compatible change**
    
    
    
    This PR includes breaking changes to the following core packages:
    
    - none
    
    This PR includes breaking changes to the follow extensions:
    
    - none
    
    ## Dependencies
    
    ### Adds
    
    - none
    
    ### Removes
    
    - none

---
+++ sources/@roots/bud-tailwindcss/src/extension.unit.test.ts
@@ -0,0 +1,177 @@
+import {beforeEach, describe, expect, it, jest} from '@jest/globals'
+// @ts-ignore
+import resolveConfig from 'tailwindcss/resolveConfig'
+
+import Extension from './extension'
+
+jest.mock(`@roots/bud-postcss`)
+
+const Logger = {
+  error: jest.fn(),
+  success: jest.fn(),
+  scope: [],
+  instance: {
+    error: jest.fn(),
+    success: jest.fn(),
+    scope: () => Logger,
+  },
+}
+
+// @ts-ignore
+const PostCSS = {
+  setPlugins: jest.fn(),
+}
+
+const Bud = {
+  extensions: {add: jest.fn()},
+  hooks: {async: jest.fn()},
+  logger: Logger,
+  module: {resolve: jest.fn()},
+  postcss: PostCSS,
+  context: {
+    config: {
+      [`tailwind.config.js`]: {
+        module: {
+          content: [`**/*.stub`],
+          theme: {
+            extend: {
+              colors: {
+                primary: `red`,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+} as any
+
+describe(`@roots/bud-tailwindcss extension`, () => {
+  beforeEach(async () => {
+    jest.clearAllMocks()
+  })
+
+  it(`basic checks`, () => {
+    const extension = new Extension(Bud)
+
+    expect(extension.getOptions()).toEqual(extension.options)
+    expect(extension.options.generateImports).toBe(
+      extension.getOption(`generateImports`),
+    )
+  })
+
+  it(`should have label`, () => {
+    const extension = new Extension(Bud)
+    expect(extension.label).toBe(`@roots/bud-tailwindcss`)
+  })
+
+  it(`should not call extensions.add when generateImports is false`, async () => {
+    const extension = new Extension(Bud)
+    extension.setOption(`generateImports`, false)
+    extension.app.extensions.add = jest.fn() as any
+
+    await extension.configAfter()
+    expect(extension.options.generateImports).toBe(false)
+    expect(extension.app.extensions.add).not.toHaveBeenCalled()
+  })
+
+  it(`should attempt to resolve modules`, async () => {
+    const extension = new Extension(Bud)
+    await extension.init()
+    expect(Bud.module.resolve).toHaveBeenCalledTimes(4)
+  })
+
+  it(`should return a copy of resolveConfig return`, async () => {
+    const extension = new Extension(Bud)
+
+    const configInitial = resolveConfig(
+      Bud.context.config[`tailwind.config.js`].module,
+    )
+
+    expect(extension.theme.colors).not.toBe(configInitial.theme.colors)
+  })
+
+  it(`should produce a static module`, async () => {
+    const extension = new Extension(Bud)
+
+    expect(extension.makeStaticModule(`colors`)).toMatchSnapshot()
+  })
+
+  it(`should resolve tailwind config values`, async () => {
+    const extension = new Extension(Bud)
+
+    expect(
+      extension.resolveTailwindConfigValue(`colors`),
+    ).toMatchSnapshot()
+  })
+
+  it(`should set generateImports to an array`, async () => {
+    const extension = new Extension(Bud)
+
+    extension.generateImports([`colors`])
+    expect(extension.getOption(`generateImports`)).toStrictEqual([
+      `colors`,
+    ])
+  })
+
+  it(`should set importable values`, async () => {
+    const extension = new Extension(Bud)
+    const importables = undefined
+    extension.generateImports(importables)
+    expect(extension.importableKeys).toMatchSnapshot()
+  })
+
+  it(`should set importables when generateImports is called with array arg`, async () => {
+    const extension = new Extension(Bud)
+    const importables = [`colors`]
+    extension.generateImports(importables)
+    expect(extension.importableKeys).toBe(importables)
+  })
+
+  it(`should set importables when generateImports is called with false arg`, async () => {
+    const extension = new Extension(Bud)
+    const importables = false
+    extension.generateImports(importables)
+    expect(extension.importableKeys).toMatchSnapshot()
+  })
+
+  it(`should set importables when generateImports is called with true arg`, async () => {
+    const extension = new Extension(Bud)
+
+    extension.generateImports(true)
+    expect(extension.getOption(`generateImports`)).toBe(true)
+  })
+
+  it(`should call extensions.add when generateImports is not undefined`, async () => {
+    const extension = new Extension(Bud)
+
+    extension.generateImports(true)
+    await extension.configAfter()
+    expect(Bud.extensions.add).toHaveBeenCalled()
+  })
+
+  it(`should register alias when generateImports is not undefined`, async () => {
+    const extension = new Extension(Bud)
+
+    await extension.configAfter()
+    expect(Bud.hooks.async).toHaveBeenCalledTimes(1)
+    expect(Bud.hooks.async).toHaveBeenCalledWith(
+      `build.resolve.alias`,
+      expect.any(Function),
+    )
+  })
+
+  it(`should call postcss.setPlugins`, async () => {
+    const extension = new Extension(Bud)
+
+    await extension.configAfter()
+    expect(PostCSS.setPlugins).toHaveBeenCalled()
+  })
+
+  it(`should log success after setting plugin`, async () => {
+    const extension = new Extension(Bud)
+
+    await extension.configAfter()
+    expect(Logger.success).toHaveBeenCalledTimes(1)
+  })
+})
