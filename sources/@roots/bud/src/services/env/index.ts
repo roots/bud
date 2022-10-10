36eaf82533c2c3441f5924c608feedd068213015
Kelly Mears <developers@tinypixel.dev>
2022-10-10 02:12:51 -0700

    fix: service container

--- sources/@roots/bud/src/services/env/index.ts
+++ sources/@roots/bud/src/services/env/index.ts
@@ -1,84 +1,18 @@
-import type {Bud} from '@roots/bud-framework'
-import {Service} from '@roots/bud-framework/service'
-import type {Env as Base} from '@roots/bud-framework/services'
+import {ServiceContainer} from '@roots/bud-framework/service'
 import {bind} from '@roots/bud-support/decorators'
-import Container from '@roots/container'
 
 /**
  * Env service
  *
  * @public
  */
-export default class Env extends Service implements Base.Service {
+export default class Env extends ServiceContainer {
   /**
    * Service label
    *
    * @public
    */
   public static label = `env`
-
-  /**
-   * Env data
-   *
-   * @public
-   */
-  public data: Container
-
-  /**
-   * Has env value
-   *
-   * @public
-   * @deprecated use {@link Env.data.has} instead
-   */
-  public has: Container['has']
-
-  /**
-   * Get env value
-   *
-   * @public
-   * @deprecated use {@link Env.data.get} instead
-   */
-  public get: Container['get']
-
-  /**
-   * Set env value
-   *
-   * @public
-   * @deprecated Use {@link Env.data.set} instead
-   */
-  public set: Container['set']
-
-  /**
-   * Is env value a string
-   *
-   * @public
-   * @deprecated use {@link Env.data.isString} instead
-   */
-  public isString: Container['isString']
-
-  /**
-   * Is env value a function
-   *
-   * @public
-   * @deprecated use {@link Env.data.isFunction} instead
-   */
-  public isFunction: Container['isFunction']
-
-  /**
-   * Class constructor
-   *
-   * @public
-   */
-  public constructor(app: Bud) {
-    super(app)
-    this.data = new Container()
-
-    this.has = this.data.has
-    this.get = this.data.get
-    this.set = this.data.set
-    this.isFunction = this.data.isFunction
-    this.isString = this.data.isString
-  }
 
   /**
    * Bootstrap event callback
@@ -88,7 +22,7 @@ export default class Env extends Service implements Base.Service {
    */
   @bind
   public async bootstrap() {
-    this.data.setStore(this.app.context.env)
+    this.setStore(this.app.context.env)
   }
 
   /**
@@ -100,8 +34,7 @@ export default class Env extends Service implements Base.Service {
    */
   @bind
   public getPublicEnv(): Record<string, any> {
-    return this.data
-      .getEntries()
+    return this.getEntries()
       .filter(this.filterPublicEnv)
       .map(this.transformPublicEnv)
       .reduce((a, [k, v]) => ({...a, [k]: v}), {})
