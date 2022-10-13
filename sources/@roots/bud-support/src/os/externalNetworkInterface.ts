1492e336fecae6da88091819cbdba0fb466eca3a
Kelly Mears <developers@tinypixel.dev>
2022-10-13 01:54:19 -0700

    add fallback to 0.0.0.0

--- sources/@roots/bud-support/src/os/externalNetworkInterface.ts
+++ sources/@roots/bud-support/src/os/externalNetworkInterface.ts
@@ -1,7 +1,8 @@
 import {networkInterfaces} from 'node:os'
 
-export const ipv4 = Object.values(networkInterfaces())
-  .flat()
-  .find(i => i?.family === `IPv4` && !i?.internal)?.address
+export const ipv4 =
+  Object.values(networkInterfaces())
+    .flat()
+    .find(i => i?.family === `IPv4` && !i?.internal)?.address || `0.0.0.0`
 
-export const ipv4Url = (ip: string) => new URL(`http://${ip}`)
+export const ipv4Url = new URL(`http://${ipv4}`)
