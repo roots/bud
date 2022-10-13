8c6054f42bb76c2e6e71a4f0f137bfe2d70a84a6
Kelly Mears <developers@tinypixel.dev>
2022-10-13 00:27:42 -0700

    tests

---
+++ sources/@roots/bud/src/index.test.ts
@@ -0,0 +1,21 @@
+import {describe, expect, it} from '@jest/globals'
+
+import Default, {Bud, factory, get} from './index'
+
+describe(`@roots/bud`, () => {
+  it(`should have a default export that is Bud`, () => {
+    expect(Default).toBe(Bud)
+  })
+
+  it(`should have a named Bud export`, () => {
+    expect(Bud).toBeInstanceOf(Function)
+  })
+
+  it(`should have a named factory export`, () => {
+    expect(factory).toBeInstanceOf(Function)
+  })
+
+  it(`should have a named get export`, () => {
+    expect(get).toBeInstanceOf(Function)
+  })
+})
