2f7c40fff551d44e4626b55d5e9208d5c91165b5
Kelly Mears <developers@tinypixel.dev>
2022-10-22 23:11:23 -0700

    issue-1798

--- sources/@roots/bud-sass/test/error.test.ts
+++ sources/@roots/bud-sass/test/error.test.ts
@@ -1,48 +1,44 @@
 /* eslint-disable no-console */
-import { describe, expect, it } from "@jest/globals";
-import { execa } from "execa";
+import {describe, expect, it} from '@jest/globals'
+import {execa} from 'execa'
 
 describe(`@roots/bud-sass`, () => {
   it(`should not throw in development`, async () => {
+    let error
     try {
-      await execa(`yarn`, [
+      const child = await execa(`yarn`, [
         `workspace`,
-        `@tests/sass-error`,
+        `@tests/issue-1798`,
         `run`,
         `bud`,
-        `build`,
-        `--mode`,
-        `development`,
-      ]);
+        `dev`,
+      ])
     } catch (e) {
-      expect(e.message.split(`\n`).pop()).toMatchInlineSnapshot(
-        `"… watching project sources                                      ℹ [2mctrl+c to exit[22m"`
-      );
+      error = e.message?.split(`\n`).pop()
     }
-  });
+
+    expect(error).toMatchInlineSnapshot(
+      `"… watching project sources                                      ℹ [2mctrl+c to exit[22m"`,
+    )
+  })
 
   it(`should throw in production`, async () => {
+    let error
     try {
-      const { stdout } = await execa(`yarn`, [
+      const {stdout} = await execa(`yarn`, [
         `workspace`,
-        `@tests/sass-error`,
+        `@tests/issue-1798`,
         `run`,
         `bud`,
         `build`,
-      ]);
-      expect(stdout.split(`\n`).splice(0, 10).join(`\n`))
-        .toMatchInlineSnapshot(`
-              "
-              [38;2;255;92;87m✘[39m  @tests/sass-error [38;2;87;199;255m./dist[39m [2m[96e31d10feca25b55837][22m
-              [2m│[22m
-              [2m├─[22m [38;2;255;92;87m✘[39m [38;2;255;92;87merror[39m
-              [2m│[22m [7mSassError: SassError: Expected escape sequence.[27m
-              [2m│[22m   ╷
-              [2m│[22m 1 │ \\
-              [2m│[22m   │  ^
-              [2m│[22m   ╵
-              [2m│[22m   src/index.scss 1:2  root stylesheet"
-          `);
-    } catch (e) {}
-  });
-});
+      ])
+
+      expect(stdout.split(`\n`)[4]).toMatchInlineSnapshot(
+        `"[2m│[22m [7mSassError: SassError: Expected escape sequence.[27m"`,
+      )
+    } catch (e) {
+      error = e
+    }
+    expect(error).toBeUndefined()
+  })
+})
