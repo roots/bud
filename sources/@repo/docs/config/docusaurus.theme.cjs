73d6f5d34220c88acb24ab95576d6d5d3392ecde
Kelly Mears <developers@tinypixel.dev>
2022-11-09 19:53:59 -0800

    🧹

--- sources/@repo/docs/config/docusaurus.theme.cjs
+++ sources/@repo/docs/config/docusaurus.theme.cjs
@@ -136,6 +136,7 @@ const navbar = {
       label: `Releases`,
       position: `left`,
       items: [
+        {to: `/releases/tags/6-6`, label: `6.6`},
         {to: `/releases/tags/6-5`, label: `6.5`},
         {to: `/releases/tags/6-4`, label: `6.4`},
         {to: `/releases/tags/6-3`, label: `6.3`},
