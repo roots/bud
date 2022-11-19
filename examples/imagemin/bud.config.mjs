43de2b9b546543a7f3ce24506a9d3a4967e1e0c8
Kelly Mears <developers@tinypixel.dev>
2022-11-19 11:18:21 -0800

    update examples

--- examples/imagemin/bud.config.mjs
+++ examples/imagemin/bud.config.mjs
@@ -2,9 +2,6 @@
  * @type {(app: import('@roots/bud').Bud) => Promise<unknown>}
  */
 export default async app => {
-  app
-    .entry(`app`, `app.js`)
-    .minimize()
-    .encode(`jpg`, {quality: 75})
-    .encode(`oxipng`, {quality: 100})
+  app.entry(`app`, `app.js`).minimize()
+  imagemin.encode(`jpg`, {quality: 75}).encode(`png`, {quality: 100})
 }
