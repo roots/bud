43ec015d42b64159015c07e74f78191d1e6bc387
Kelly Mears <developers@tinypixel.dev>
2022-11-21 13:28:33 -0800

    fix: jank browser

--- sources/@roots/browserslist-config/scripts/index.mjs
+++ sources/@roots/browserslist-config/scripts/index.mjs
@@ -1,5 +1,20 @@
 import {generate} from './generate.mjs'
 
-await generate(`broad.cjs`, [`last 4 versions`, `not dead`])
-await generate(`current.cjs`, [`last 2 versions`, `not dead`])
-await generate(`index.cjs`, [`last 3 versions`, `not dead`])
+await generate(`broad.cjs`, [
+  `last 4 chrome major versions`,
+  `last 4 safari major versions`,
+  `last 4 firefox major versions`,
+  `not dead`,
+])
+await generate(`current.cjs`, [
+  `last 2 chrome major versions`,
+  `last 2 safari major versions`,
+  `last 2 firefox major versions`,
+  `not dead`,
+])
+await generate(`index.cjs`, [
+  `last 3 chrome major versions`,
+  `last 3 safari major versions`,
+  `last 3 firefox major versions`,
+  `not dead`,
+])
