e3f62baee0dadbc903534659bcb8c0e21d6d8fc5
Kelly Mears <developers@tinypixel.dev>
2022-12-12 03:10:45 -0800

    tests

--- tests/reproductions/issue-1886/src/index.js
+++ tests/reproductions/issue-1886/src/index.js
@@ -1,7 +1,7 @@
 import './index.css'
 
-import webp from './bud.png?as=webp'
-import webp50 from './bud-50.png?as=webp@50'
-import jpg from './bud.png?as=jpg'
+import webp from '@src/images/bud.png?as=webp'
+import webp50 from '@src/images/bud-50.png?as=webp@50'
+import jpg from '@src/images/bud.png?as=jpg'
 
 console.log(webp, jpg, webp50)
