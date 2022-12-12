684b9ef9f56d958cc7d2da0753217e7e05785fff
Kelly Mears <developers@tinypixel.dev>
2022-12-12 03:10:29 -0800

    examples

--- examples/purgecss/bud.config.cjs
+++ examples/purgecss/bud.config.cjs
@@ -1,7 +1,11 @@
 module.exports = async app =>
   app
-    .template({template: 'public/index.html'})
-    .entry({app: 'app.css'})
+    .template({
+      template: app.path('public/index.html'),
+    })
+    .entry({
+      app: 'app.css',
+    })
     .purgecss({
       content: [app.path('public/*.html')],
       css: [app.path('src/**/*.css')],
