b4e40378158e56f633fef7d8b16b735b5aacad66
Kelly Mears <developers@tinypixel.dev>
2023-01-02 12:03:47 -0800

    🩹 fix: `server is not a constructor` (#1976)
    Also removes `react` from `@roots/bud-support` peerDependencies (since we don't want it overridden there, ref #1974)
    
    refers:
    
    - Fixes #1975
    
    ## Type of change
    
    **PATCH: backwards compatible change**
    
    
    
    This PR includes breaking changes to the following core packages:
    
    - none
    
    This PR includes breaking changes to the follow extensions:
    
    - none
    
    ## Dependencies
    
    ### Adds
    
    - none
    
    ### Removes
    
    - none

--- sources/@roots/bud-server/src/server/https.ts
+++ sources/@roots/bud-server/src/server/https.ts
@@ -1,7 +1,7 @@
 import type {RequestListener} from 'node:http'
 import {createServer, Server as HttpsServer} from 'node:https'
 
-import type {Server} from '@roots/bud-framework/services'
+import type {Connection} from '@roots/bud-framework/services/server'
 import {BaseServer} from '@roots/bud-server/server/base'
 import {bind} from '@roots/bud-support/decorators'
 
@@ -10,7 +10,7 @@ import {bind} from '@roots/bud-support/decorators'
  *
  * @public
  */
-export class Https extends BaseServer implements Server.Connection {
+export class Server extends BaseServer implements Connection {
   /**
    * Server instance
    *
