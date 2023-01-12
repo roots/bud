977d81020fe19b874d70296a88275a52adcf1d7f
Kelly Mears <developers@tinypixel.dev>
2023-01-12 15:29:00 -0800

    prettier fix

--- sources/@roots/bud-api/src/methods/html/index.ts
+++ sources/@roots/bud-api/src/methods/html/index.ts
@@ -4,7 +4,8 @@ import type * as HTMLWebpackPlugin from '@roots/bud-support/html-webpack-plugin'
 import type * as InterpolateHTMLExtension from './interpolate-html-webpack-plugin/index.js'
 
 export type Parameters = [
-  (HTMLWebpackPlugin.Options & InterpolateHTMLExtension.Options & {filename?: string})?,
+  (HTMLWebpackPlugin.Options &
+    InterpolateHTMLExtension.Options & {filename?: string})?,
 ]
 
 export interface html {
@@ -18,10 +19,17 @@ export const html: html = async function (this: Bud, options) {
   const helpers = await import(`./helpers.js`)
 
   const {Plugin} = await import(`@roots/bud-support/html-webpack-plugin`)
-  const {InterpolateHtmlWebpackPlugin} = await import(`./interpolate-html-webpack-plugin/index.js`)
+  const {InterpolateHtmlWebpackPlugin} = await import(
+    `./interpolate-html-webpack-plugin/index.js`
+  )
 
-  const htmlPlugin = new Plugin(helpers.getHtmlPluginOptions(this, options))
-  const interpolatePlugin = new InterpolateHtmlWebpackPlugin(Plugin.getHooks, helpers.getInterpolatePluginOptions(this, options))
+  const htmlPlugin = new Plugin(
+    helpers.getHtmlPluginOptions(this, options),
+  )
+  const interpolatePlugin = new InterpolateHtmlWebpackPlugin(
+    Plugin.getHooks,
+    helpers.getInterpolatePluginOptions(this, options),
+  )
 
   this.hooks.async(`build.plugins`, async (plugins = []) => {
     plugins.push(htmlPlugin, interpolatePlugin)
