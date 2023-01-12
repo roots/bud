e74d490123fb4e2900ae655dc2507f8f00523819
Kelly Mears <developers@tinypixel.dev>
2023-01-11 20:32:03 -0800

    📁 types: improve @roots/bud-swc typings

--- sources/@roots/bud-swc/src/types/index.ts
+++ sources/@roots/bud-swc/src/types/index.ts
@@ -2,7 +2,7 @@
 /// <reference types="@roots/bud-extensions" />
 
 import type {Build} from '@roots/bud-framework/services'
-import type BudSWC from '@roots/bud-swc/extension'
+import type BudSWC from '@roots/bud-swc'
 
 declare module '@roots/bud-framework' {
   interface Bud {
