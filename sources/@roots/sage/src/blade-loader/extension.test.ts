9859221a662b03f0f2d195913355cadfc59f4230
Kelly Mears <developers@tinypixel.dev>
2023-01-16 14:12:44 -0800

    🧪 fix unused imports

--- sources/@roots/sage/src/blade-loader/extension.test.ts
+++ sources/@roots/sage/src/blade-loader/extension.test.ts
@@ -1,15 +1,7 @@
-import {Bud, factory} from '@repo/test-kit/bud'
-import {beforeEach, describe, expect, it, vi} from 'vitest'
-
+import {describe, expect, it} from 'vitest'
 import {BladeLoaderExtension} from './extension.js'
 
 describe(`@roots/sage/blade-loader`, () => {
-  let bud: Bud
-
-  beforeEach(async () => {
-    bud = await factory()
-  })
-
   it(`should be constructable`, () => {
     expect(BladeLoaderExtension).toBeInstanceOf(Function)
   })
