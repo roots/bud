62cdb8088eb4b0588c6b9486cc4578b1df9544ba
Kelly Mears <developers@tinypixel.dev>
2023-01-16 09:58:25 -0800

    🩹 fix: --cwd flag (#2008)
    - Fix: `--basedir`/`--cwd` flag handling in `@roots/bud/context`, `@roots/bud/factory`, and `@roots/bud/cli/commands/bud`
    - Fix: need to pass config to tailwindcss (tailwindcss doesn't care about webpack context and will always try to find the tailwind config in the process cwd).
    
    refers:
    
    - none
    
    ## Type of change
    
    **PATCH: backwards compatible change**
    
    
    
    This PR includes breaking changes to the following core packages:
    
    - none
    
    This PR includes breaking changes to the follow extensions:
    
    - none
    
    ## Dependencies
    
    ### Adds
    
    - none
    
    ### Removes
    
    - none

---
+++ sources/@roots/bud-tailwindcss/test/implementation.test.ts
@@ -0,0 +1,34 @@
+import {join} from 'node:path'
+import {paths} from '@repo/constants'
+import execa, {ExecaReturnValue} from '@roots/bud-support/execa'
+import {beforeAll, describe, expect, it} from 'vitest'
+import fs from '@roots/bud-support/fs-jetpack'
+
+describe(`@tests/tailwind-implementation`, () => {
+  let child: ExecaReturnValue
+
+  beforeAll(async () => {
+    await execa(`yarn`, [`bud`, `clean`, `--cwd`, `sources/@roots/bud-tailwindcss/test/implementation`])
+
+    try {
+      child = await execa(`yarn`, [`bud`, `build`, `--no-cache`, `--debug`, `--cwd`, `sources/@roots/bud-tailwindcss/test/implementation`])
+    } catch (error) {}
+  }, 30000)
+
+  it(`should generate bg-primary class`, async () => {
+    expect(
+      await fs.existsAsync(
+        join(
+          paths.sources, `@roots`, `bud-tailwindcss`, `test`, `implementation`, `dist`, `css`, `main.css`
+        ),
+      ),
+    ).toBe(`file`)
+    expect(
+      await fs.readAsync(
+       join(
+        paths.sources, `@roots`, `bud-tailwindcss`, `test`, `implementation`, `dist`, `css`, `main.css`
+       )
+      )
+    ).toEqual(expect.stringContaining(`.bg-primary {`))
+  })
+})
