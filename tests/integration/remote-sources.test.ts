69d3d63a0a7a2bc09fe589d6c5dbef2ae7d43387
Kelly Mears <developers@tinypixel.dev>
2023-02-07 03:44:54 -0800

    🧪 test: fix snap

--- tests/integration/remote-sources.test.ts
+++ tests/integration/remote-sources.test.ts
@@ -1,38 +1,27 @@
 import {Project} from '@repo/test-kit/project'
-import {beforeAll, describe, expect, it} from 'vitest'
+import {describe, expect, it} from 'vitest'
 
-const run = pacman => () => {
-  let project: Project
+let project: Project
 
-  beforeAll(async () => {
-    project = await new Project({
-      label: `@examples/remote-sources`,
-      with: pacman,
-    }).setup()
+describe(`app.js`, async () => {
+  project = await new Project({
+    label: `@examples/remote-sources`,
+    with: `npm`,
+  }).setup()
+
+  it(`app.js should not be empty`, () => {
+    expect(project.assets[`app.js`].length).toBeGreaterThan(10)
   })
 
-  describe(`app.js`, () => {
-    it(`should not be empty`, () => {
-      expect(project.assets[`app.js`].length).toBeGreaterThan(10)
-    })
-
-    it(`should not contain import statements`, () => {
-      expect(project.assets[`app.js`].includes(`import `)).toBeFalsy()
-    })
-
-    it(`should be present in the manifest`, () => {
-      expect(project.manifest[`app.js`]).toMatchSnapshot()
-    })
+  it(`app.js should not contain import statements`, () => {
+    expect(project.assets[`app.js`].includes(`import `)).toBeFalsy()
   })
 
-  describe(`manifest.json`, () => {
-    it(`should have expected number of items`, () => {
-      expect(Object.keys(project.manifest)).toHaveLength(3)
-    })
+  it(`app.js should be present in the manifest`, () => {
+    expect(project.manifest[`app.js`]).toMatchSnapshot()
   })
-}
 
-describe(`remote-sources`, () => {
-  describe(`npm`, run(`npm`))
-  describe(`yarn`, run(`yarn`))
+  it(`manifest.json should have expected number of items`, () => {
+    expect(Object.keys(project.manifest)).toHaveLength(4)
+  })
 }, 240000)
