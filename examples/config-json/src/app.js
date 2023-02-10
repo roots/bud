492960f53a78dd64ed6237efb553e438e28181fb
Kelly Mears <developers@tinypixel.dev>
2023-02-10 14:14:18 -0800

    ✨ improve(@roots/bud): static config parsing

---
+++ examples/config-json/src/app.js
@@ -0,0 +1,5 @@
+import './global.css'
+
+document.querySelector('#root')?.classList.add('init')
+
+module?.hot?.accept(console.error)
--- sources/@roots/bud-framework/src/configuration/configuration.ts
+++ sources/@roots/bud-framework/src/configuration/configuration.ts
@@ -1,5 +1,9 @@
 import {bind} from '@roots/bud-support/decorators'
+import get from '@roots/bud-support/lodash/get'
+import isArray from '@roots/bud-support/lodash/isArray'
 import isFunction from '@roots/bud-support/lodash/isFunction'
+import isObject from '@roots/bud-support/lodash/isObject'
+import isString from '@roots/bud-support/lodash/isString'
 
 import type {Bud} from '../bud.js'
 import type {File} from '../types/options/context.js'
@@ -42,22 +46,52 @@ class Configuration {
 
   /**
    * Process static configuration
-   *
-   * @public
-   * @decorator `@bind`
    */
   @bind
   public async staticConfig(
-    description: Record<string, any>,
+    description: File,
   ): Promise<unknown> {
     this.bud.log(`processing as static configuration:`, description.name)
 
     return await Promise.all(
       Object.entries(description.module).map(async ([key, value]) => {
-        const request = this.bud[key]
-        if (isFunction(request)) await request(value)
+        await this.handleConfigEntry(this.bud, [key, value])
       }),
     )
+  }
+
+  @bind
+  public async handleConfigEntry(
+    obj: any,
+    [key, value]: [string, unknown],
+  ) {
+    if (!(key in obj)) return
+
+    const request = obj[key]
+    const normalValue = isArray(value) ? value : [value]
+
+    const parsedValue = normalValue.map((v) => {
+      if (isString(v) && (v.startsWith(`_app.`) || v.startsWith(`_bud.`))) {
+        return get(this.bud, v.replace(`_app.`, ``).replace(`_bud.`, ``).trim())
+      }
+      if (isString(v) && (v.startsWith(`bud =>`) || v.startsWith(`app =>`))) {
+        return eval(v.trim())(this.bud)
+      }
+      if (isString(v) && v.startsWith(`=>`)) {
+        return eval(v.slice(3))
+      }
+      return v
+    })
+
+    if (isFunction(request))
+      await request(...(parsedValue))
+
+    if (isObject(request))
+      await Promise.all(
+        Object.entries(value).map(async ([key, value]) => {
+          return await this.handleConfigEntry(request, [key, value])
+        }),
+      )
   }
 }
 
