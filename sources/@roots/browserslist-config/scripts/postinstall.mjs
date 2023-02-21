5bc9cb3b0eead473f4116e97f155ff32e595ee6b
Kelly Mears <developers@tinypixel.dev>
2023-02-20 22:32:34 -0800

    ✨ improve(@roots/bud): safer postinstall scripts

--- sources/@roots/browserslist-config/scripts/postinstall.mjs
+++ sources/@roots/browserslist-config/scripts/postinstall.mjs
@@ -5,16 +5,12 @@
 
 import {execaCommandSync} from 'execa'
 
-if (process.env.npm_package_version === `0.0.0`) {
-  process.exit(0)
-}
-
-execaCommandSync(`npx browserslist --update-db`, {
-  cwd: process.env.INIT_CWD ?? process.cwd(),
-  reject: false,
-  timeout: 10000,
-})
-
-process.exit(0)
+try {
+  execaCommandSync(`npx browserslist --update-db`, {
+    cwd: process.env.INIT_CWD ?? process.cwd(),
+    reject: false,
+    timeout: 10000,
+  })
+} catch (e) {}
 
 export {}
