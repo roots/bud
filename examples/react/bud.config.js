22ff919e811a81cb1951a3ec0e779078df929665
Kelly Mears <developers@tinypixel.dev>
2023-04-02 21:08:41 -0700

    🧪 test: set examples/react runtime to `single`

--- examples/react/bud.config.js
+++ examples/react/bud.config.js
@@ -1,5 +1,6 @@
 export default async app => {
-  app.entry({app: [`app.js`, `app.css`]}).when(app.isProduction, () => {
-    app.splitChunks().minimize()
-  })
+  app
+    .entry(`app`, [`app.js`, `app.css`])
+    .runtime(`single`)
+    .when(app.isProduction, () => app.splitChunks().minimize())
 }
