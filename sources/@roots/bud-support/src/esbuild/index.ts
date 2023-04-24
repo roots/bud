2d30770e8750373d41cfc40cd63ca3afd5075fa2
Kelly Mears <developers@tinypixel.dev>
2023-04-23 22:26:16 -0400

    ✨ improve(@roots/bud-support): resolve esbuild-wasm

--- sources/@roots/bud-support/src/esbuild/index.ts
+++ sources/@roots/bud-support/src/esbuild/index.ts
@@ -1,12 +1,44 @@
-let esbuild: typeof import('esbuild-wasm')
+import {resolve} from 'import-meta-resolve'
 
-export const getImplementation = async () => {
-  if (esbuild) return esbuild
-  try {
-    esbuild = await import(`esbuild`)
-  } catch (err) {
-    esbuild = await import(`esbuild-wasm`)
+import logger from '../logger/index.js'
+
+let path: string
+
+let transformer: typeof import('esbuild-wasm')
+
+export const getImplementation = async (context: string) => {
+  if (transformer) return transformer
+
+  const sources: Array<[string, string]> = [
+    [`esbuild`, context],
+    [`esbuild`, import.meta.url],
+    [`esbuild-wasm`, context],
+    [`esbuild-wasm`, import.meta.url],
+  ]
+
+  await sources.reduce(async (promised, props) => {
+    await promised
+    if (transformer) return
+    await trySource(...props)
+  }, Promise.resolve())
+
+  if (!transformer) {
+    throw new Error(`Neither esbuild nor esbuild-wasm could be found.`)
   }
 
-  return esbuild
+  return transformer
+}
+
+async function trySource(signifier: string, context: any) {
+  try {
+    path = await resolve(signifier, context)
+    logger.log(
+      `using transformer:`,
+      path,
+      `resolved from context:`,
+      context,
+    )
+    transformer = await import(path)
+    return transformer
+  } catch (err) {}
 }
