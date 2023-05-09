d0975e3e6a139dcf6b1250903e03d7cac167862c
Kelly Mears <developers@tinypixel.dev>
2023-05-09 03:21:06 -0400

    📦 deps: rollup aws deps

--- .rollup.cache/Users/kellymears/code/git/roots/bud/sources/@roots/filesystem/src/yml.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const read: (file: string) => Promise<any>;
-export declare const write: (file: string, data: any) => Promise<void>;
-export declare const parse: (source: string) => Promise<any>;
-//# sourceMappingURL=yml.d.ts.map
