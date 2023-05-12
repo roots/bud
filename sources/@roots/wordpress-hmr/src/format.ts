c64180e13385d826244d8b9f32e838aafb40beb6
Kelly Mears <developers@tinypixel.dev>
2023-05-12 18:06:00 -0400

    🗂️ types: fix @wordpress/rich-text typings

--- sources/@roots/wordpress-hmr/src/format.ts
+++ sources/@roots/wordpress-hmr/src/format.ts
@@ -1,27 +1,48 @@
+/* eslint-disable no-console */
 import {select} from '@wordpress/data'
 import {
-  FormatConfiguration,
   registerFormatType,
   unregisterFormatType,
 } from '@wordpress/rich-text'
 
-export interface Props extends FormatConfiguration {
+export interface Props {
   name: string
+  tagName: string
+  className: string
+  title: string
+  edit: CallableFunction
 }
 
-export const isRegistered = (name: string) => {
-  return select(`core/rich-text`)
+export const isRegistered = (name: string) =>
+  select(`core/rich-text`)
     .getFormatTypes()
     .some(({name: registered}) => registered === name)
-}
 
 export const register = ({name, ...settings}: Props) => {
+  if (!settings) {
+    console.error(
+      `@roots/wordpress-hmr: format settings are required`,
+      `tried to register`,
+      name,
+    )
+  }
+
+  if (!name) {
+    console.error(
+      `@roots/wordpress-hmr: format name is required`,
+      `tried to register`,
+      settings,
+    )
+  }
+
   if (!name || !settings) return
+
   isRegistered(name) && unregister({name})
-  registerFormatType(name, settings)
+  registerFormatType(name, {name, ...settings})
 }
 
 export const unregister = ({name}) => {
   if (!name) return
+
   unregisterFormatType(name)
 }
