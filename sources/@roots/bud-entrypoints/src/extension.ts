b660ecb2c6048f1e0267e3ae54bdee7d71d196a1
Kelly Mears <developers@tinypixel.dev>
2023-05-15 01:38:48 -0400

    🏎️ lazy load @roots/entrypoints-webpack-plugin

--- sources/@roots/bud-entrypoints/src/extension.ts
+++ sources/@roots/bud-entrypoints/src/extension.ts
@@ -1,18 +1,27 @@
-import {Extension} from '@roots/bud-framework'
+import type {Bud} from '@roots/bud-framework'
+import {Extension} from '@roots/bud-framework/extension'
 import {
   expose,
   label,
   options,
-  plugin,
 } from '@roots/bud-framework/extension/decorators'
 import type {Options} from '@roots/entrypoints-webpack-plugin'
-import {EntrypointsWebpackPlugin} from '@roots/entrypoints-webpack-plugin'
+import type {EntrypointsWebpackPlugin} from '@roots/entrypoints-webpack-plugin'
 
 @label(`@roots/bud-entrypoints`)
 @expose(`entrypoints`)
 @options({emitHtml: false})
-@plugin(EntrypointsWebpackPlugin)
 export class BudEntrypoints extends Extension<
   Options,
   EntrypointsWebpackPlugin
-> {}
+> {
+  /**
+   * {@label Extension.make}
+   */
+  public override async make?(bud: Bud, options: Options) {
+    const {EntrypointsWebpackPlugin} = await this.import(
+      `@roots/entrypoints-webpack-plugin`,
+    )
+    return new EntrypointsWebpackPlugin(options)
+  }
+}
