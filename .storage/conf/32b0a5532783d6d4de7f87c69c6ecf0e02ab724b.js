b1fe73b42b5d9e84fbf24e47256b67e7d7cb5113
Kelly Mears <developers@tinypixel.dev>
2023-05-16 02:58:00 -0400

    🧪 test: fix

--- .storage/conf/32b0a5532783d6d4de7f87c69c6ecf0e02ab724b.js
+++
@@ -1,7 +0,0 @@
-export default {
-  test: {
-    hookTimeout: 24e4,
-    testTimeout: 24e4,
-    include: [`tests/integration/**/*.test.ts`]
-  }
-};
--- .storage/conf/a413e6a0efe6138c6cc181f44e64e0db261f99ae.js
+++
@@ -1,8 +0,0 @@
-export default {
-  test: {
-    hookTimeout: 24e4,
-    testTimeout: 24e4,
-    include: [`tests/e2e/*.test.ts`],
-    threads: false
-  }
-};
