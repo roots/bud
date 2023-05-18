8d675ee3f5ba096459efd26cf75a172f19ff2982
Kelly Mears <developers@tinypixel.dev>
2023-05-18 14:27:42 -0400

    🏎️ optimize: performance (build)

--- tests/util/project/.storage/conf/063c9a20d31edc8912ba5218a7d8de2212641939.js
+++
@@ -1,9 +0,0 @@
-export default async (bud) => {
-  bud.assets(`fonts`).entry(`app`, [`scripts/app`, `styles/app`]).watch([bud.path(`@src`, `*.html`), bud.path(`@src`, `images`)]).serve(3015).splitChunks(false).minimize(false).html({
-    template: bud.path(`@src`, `index.html`),
-    replace: {
-      noScript: `You need to enable JavaScript to run this app`
-    }
-  });
-  console.log(`foo`, `bar`);
-};
