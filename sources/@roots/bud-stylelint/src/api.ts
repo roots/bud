ed5239016df7e244af86580c8db40ff0afa28613
Kelly Mears <developers@tinypixel.dev>
2023-06-05 13:26:52 -0400

    🩹 fix: types (#2302)
    Fix types for:
    - `bud.tailwind.setTheme`
    - `bud.stylelint`
    
    ## Type of change
    
    **PATCH: backwards compatible change**

---
+++ sources/@roots/bud-stylelint/src/api.ts
@@ -0,0 +1,400 @@
+import {
+  DynamicOption,
+  Extension,
+  type StrictPublicExtensionApi as PublicExtensionApi,
+} from '@roots/bud-framework/extension'
+import {bind, options} from '@roots/bud-framework/extension/decorators'
+import Plugin from 'stylelint-webpack-plugin'
+
+export type Options = Plugin.Options &
+  Record<string, unknown> & {
+    cache: Plugin.Options['cache']
+    cacheLocation: Plugin.Options['cacheLocation']
+    config: Plugin.Options[`config`]
+    context: Plugin.Options['context']
+    extensions: Plugin.Options['extensions']
+    failOnError: Plugin.Options['failOnError']
+    failOnWarning: Plugin.Options['failOnWarning']
+    files: Plugin.Options[`files`]
+    fix: Plugin.Options['fix']
+    formatter: Plugin.Options['formatter']
+    lintDirtyModulesOnly: Plugin.Options['lintDirtyModulesOnly']
+    stylelintPath: Plugin.Options[`stylelintPath`]
+    threads: Plugin.Options['threads']
+  }
+
+export type Api = PublicExtensionApi<BudStylelintPublicApi, Options> & {
+  rules: Options[`config`][`rules`]
+  setRules(
+    rules:
+      | Options[`config`][`rules`]
+      | ((
+          rules: Options[`config`][`rules`],
+        ) => Options[`config`][`rules`]),
+  ): Api
+  getRules(): Api[`config`][`rules`]
+  plugins: Options[`config`][`plugins`]
+  setPlugins(
+    plugins:
+      | Options[`config`][`plugins`]
+      | ((
+          plugins: Options[`config`][`plugins`],
+        ) => Options[`config`][`plugins`]),
+  ): Api
+  getPlugins(): Api[`config`][`plugins`]
+  extends(
+    config:
+      | Api[`config`][`extends`]
+      | ((configs: Api[`config`][`extends`]) => Api[`config`][`extends`]),
+  ): Api
+}
+
+@options<Options>({
+  cache: DynamicOption.make(
+    ({context, env}) => !context.ci && !env.isTrue(`CI`),
+  ),
+  cacheLocation: DynamicOption.make(({cache, path}) =>
+    path(cache.cacheDirectory, `stylelint`),
+  ),
+  config: DynamicOption.make(
+    ({context}) =>
+      Object.values(context.files ?? {}).find(({name}) =>
+        name.includes(`stylelint`),
+      )?.module ?? undefined,
+  ),
+  context: DynamicOption.make(({path}) => path(`@src`)),
+  failOnError: DynamicOption.make(({isProduction}) => isProduction),
+  failOnWarning: false,
+  files: undefined,
+  fix: false,
+  stylelintPath: undefined,
+})
+export class BudStylelintPublicApi extends Extension<Options, Plugin> {
+  /**
+   * {@link Options.config}
+   */
+  public declare config: Api[`config`]
+  /**
+   * {@link Options.config}
+   */
+  public declare getConfig: Api[`getConfig`]
+  /**
+   * {@link Options.config}
+   */
+  public declare setConfig: Api[`setConfig`]
+
+  /**
+   * {@link Options.files}
+   */
+  public declare files: Api[`files`]
+  /**
+   * {@link Options.files}
+   */
+  public declare getFiles: Api[`getFiles`]
+  /**
+   * {@link Options.files}
+   */
+  public declare setFiles: Api[`setFiles`]
+
+  /**
+   * {@link Options.failOnWarning}
+   */
+  public declare getFailOnWarning: Api['getFailOnWarning']
+
+  /**
+   * {@link Options.failOnWarning}
+   */
+  public declare setFailOnWarning: Api['setFailOnWarning']
+
+  /**
+   * {@link Options.failOnError}
+   */
+  public declare getFailOnError: Api['getFailOnError']
+
+  /**
+   * {@link Options.failOnError}
+   */
+  public declare setFailOnError: Api['setFailOnError']
+
+  /**
+   * {@link Options.cache}
+   */
+  public declare cache: Options['cache']
+
+  /**
+   * {@link Options.cache}
+   */
+  public declare getCache: Api['getCache']
+
+  /**
+   * {@link Options.cache}
+   */
+  public declare setCache: Api['setCache']
+
+  /**
+   * {@link Options.cacheLocation}
+   */
+  public declare cacheLocation: Options['cacheLocation']
+
+  /**
+   * {@link Options.cacheLocation}
+   */
+  public declare getCacheLocation: Api['getCacheLocation']
+
+  /**
+   * {@link Options.cacheLocation}
+   */
+  public declare setCacheLocation: Api['setCacheLocation']
+
+  /**
+   * {@link Options.context}
+   */
+  public declare context: Api['context']
+
+  /**
+   * {@link Options.context}
+   */
+  public declare getContext: Api['getContext']
+
+  /**
+   * {@link Options.context}
+   */
+  public declare setContext: Api['setContext']
+
+  /**
+   * {@link Options.extensions}
+   */
+  public declare extensions: Api['extensions']
+
+  /**
+   * {@link Options.extensions}
+   */
+  public declare getExtensions: Api['getExtensions']
+
+  /**
+   * {@link Options.extensions}
+   */
+  public declare setExtensions: Api['setExtensions']
+
+  /** @todo conflict with {@link BudStylelint.fix} */
+  // public declare fix: Api['fix']
+
+  /**
+   * {@link Options.fix}
+   */
+  public declare getFix: Api['getFix']
+
+  /**
+   * {@link Options.fix}
+   */
+  public declare setFix: Api['setFix']
+
+  /**
+   * {@link Options.lintDirtyModulesOnly}
+   */
+  public declare lintDirtyModulesOnly: Api['lintDirtyModulesOnly']
+
+  /**
+   * {@link Options.lintDirtyModulesOnly}
+   */
+  public declare getLintDirtyModulesOnly: Api['getLintDirtyModulesOnly']
+
+  /**
+   * {@link Options.lintDirtyModulesOnly}
+   */
+  public declare setLintDirtyModulesOnly: Api['setLintDirtyModulesOnly']
+
+  /**
+   * {@link Options.formatter}
+   */
+  public declare formatter: Api['formatter']
+
+  /**
+   * {@link Options.formatter}
+   */
+  public declare getFormatter: Api['getFormatter']
+
+  /**
+   * {@link Options.formatter}
+   */
+  public declare setFormatter: Api['setFormatter']
+
+  /**
+   * {@link Options.threads}
+   */
+  public declare threads: Api['threads']
+
+  /**
+   * {@link Options.threads}
+   */
+  public declare getThreads: Api['getThreads']
+
+  /**
+   * {@link Options.threads}
+   */
+  public declare setThreads: Api['setThreads']
+
+  /**
+   * Get stylelint rules
+   *
+   * @example
+   * ```js
+   * console.log(bud.stylelint.rules)
+   * ```
+   */
+  public get rules(): Api[`config`][`rules`] {
+    return this.config.rules
+  }
+
+  /**
+   * Set stylelint rules
+   *
+   * @example
+   * ```js
+   * bud.stylelint.rules = {'no-descending-specificity': null}
+   * ```
+   */
+  public set rules(rules: Api[`config`][`rules`]) {
+    this.setConfig((config = {}) => {
+      return {...config, rules: {...config.rules, ...rules}}
+    })
+  }
+
+  /**
+   * Get stylelint rules
+   *
+   * @example
+   * ```js
+   * console.log(bud.stylelint.getRules())
+   * ```
+   */
+  @bind
+  public getRules() {
+    return this.rules
+  }
+
+  /**
+   * Set stylelint config rules
+   *
+   * @example
+   * ```js
+   * bud.stylelint.setRules({'no-descending-specificity': null})
+   * ```
+   *
+   * @example
+   * ```js
+   * bud.stylelint.setRules(rules => ({
+   *   ...rules,
+   *  'no-descending-specificity': null
+   * }))
+   * ```
+   */
+  @bind
+  public setRules(
+    rules:
+      | Options[`config`][`rules`]
+      | ((
+          rules: Options[`config`][`rules`],
+        ) => Options[`config`][`rules`]),
+  ) {
+    this.setConfig((config = {}) => {
+      return typeof rules === `function`
+        ? {...config, rules: rules(config.rules)}
+        : {...config, rules: {...(config.rules ?? {}), ...rules}}
+    })
+    return this
+  }
+
+  /**
+   * Get stylelint plugins
+   *
+   * @example
+   * ```js
+   * console.log(bud.stylelint.plugins)
+   * ```
+   */
+  public get plugins(): Api[`config`][`plugins`] {
+    return this.config.plugins
+  }
+
+  /**
+   * Set stylelint plugins
+   *
+   * @example
+   * ```js
+   * bud.stylelint.plugins = []
+   * ```
+   */
+  public set plugins(plugins: Api[`config`][`plugins`]) {
+    this.setConfig((config = {}) => ({...config, plugins}))
+  }
+
+  /**
+   * Get stylelint plugins
+   *
+   * @example
+   * ```js
+   * console.log(bud.stylelint.getPlugins())
+   * ```
+   */
+  @bind
+  public getPlugins() {
+    return this.plugins
+  }
+
+  /**
+   * Set stylelint config plugins
+   *
+   * @example
+   * ```js
+   * bud.stylelint.setPlugins([])
+   * ```
+   *
+   * @example
+   * ```js
+   * bud.stylelint.setPlugins(plugins => [...plugins, plugin])
+   * ```
+   */
+  @bind
+  public setPlugins(
+    plugins:
+      | Options[`config`][`plugins`]
+      | ((
+          plugins: Options[`config`][`plugins`],
+        ) => Options[`config`][`plugins`]),
+  ) {
+    this.setConfig((config = {}) => {
+      return typeof plugins === `function`
+        ? {...config, plugins: plugins(config.plugins)}
+        : {...config, plugins}
+    })
+    return this
+  }
+
+  /**
+   * Extend config
+   *
+   * @example
+   * ```js
+   * bud.stylelint.extends(['@roots/bud-stylelint/config'])
+   * ```
+   *
+   * @example
+   * ```js
+   * bud.stylelint.extends(configs => [...configs, '@roots/bud-stylelint/config'])
+   * ```
+   */
+  @bind
+  public extends(
+    configs:
+      | Api[`config`][`extends`]
+      | ((configs: Api[`config`][`extends`]) => Api[`config`][`extends`]),
+  ) {
+    this.setConfig((current = {}) => {
+      return typeof configs === `function`
+        ? {...current, extends: configs(current.extends)}
+        : {...current, extends: configs}
+    })
+    return this
+  }
+}
--- sources/@roots/bud-stylelint/src/types.ts
+++ sources/@roots/bud-stylelint/src/types.ts
@@ -1,45 +1,12 @@
-/// <reference types="@roots/bud" />
-
-import type {PublicExtensionApi} from '@roots/bud-framework/extension'
-
+import type {BudStylelintPublicApi} from './api.js'
 import type {default as BudStylelint} from './extension.js'
 
 declare module '@roots/bud-framework' {
   interface Bud {
-    stylelint: PublicApi
+    stylelint: BudStylelintPublicApi
   }
 
   interface Modules {
-    '@roots/bud-stylelint': PublicApi
+    '@roots/bud-stylelint': BudStylelint
   }
-}
-
-interface PublicApi extends PublicExtensionApi<BudStylelint> {
-  cache: BudStylelint['cache']
-  cacheLocation: BudStylelint['cacheLocation']
-  config: BudStylelint['config']
-  context: BudStylelint['context']
-  fix: BudStylelint['fix']
-  stylelintPath: BudStylelint['stylelintPath']
-
-  setCache: BudStylelint['setCache']
-  getCache: BudStylelint['getCache']
-
-  setCacheLocation: BudStylelint['setCacheLocation']
-  getCacheLocation: BudStylelint['getCacheLocation']
-
-  setContext: BudStylelint['setContext']
-  getContext: BudStylelint['getContext']
-
-  setFix: BudStylelint['setFix']
-  getFix: BudStylelint['getFix']
-
-  setConfig: BudStylelint['setConfig']
-  getConfig: BudStylelint['getConfig']
-
-  failOnError: BudStylelint['failOnError']
-  failOnWarning: BudStylelint['failOnWarning']
-
-  setStylelintPath: BudStylelint['setStylelintPath']
-  getStylelintPath: BudStylelint['getStylelintPath']
 }
