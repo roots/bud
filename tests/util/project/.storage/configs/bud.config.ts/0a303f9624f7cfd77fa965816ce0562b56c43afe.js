3f40364d6b832c0cea9966944834c7e89830ddb7
Kelly Mears <developers@tinypixel.dev>
2023-07-06 05:27:00 -0400

    🗂️ typings: @roots/bud/instance (#2360)
    - 🗂️ typings: [@roots/bud/instance](https://github.com/roots/bud/commit/2f0955a6351e7e2abef0aeff44f10445bf28b5a1)
    
    ## Type of change
    
    **PATCH: backwards compatible change**

--- tests/util/project/.storage/configs/bud.config.ts/0a303f9624f7cfd77fa965816ce0562b56c43afe.js
+++
@@ -1,3 +0,0 @@
-export default async (bud) => {
-  bud.entry(`app`, [`scripts/index`, `scripts/scripts`, `styles/app`]).watch([bud.path(`@src`, `images`)]).serve(3015).html().alias(`foo`, false);
-};
