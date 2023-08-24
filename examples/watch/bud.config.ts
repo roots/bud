72b1136e6429cb006cd1291da96906ef614408ef
Kelly Mears <developers@tinypixel.dev>
2023-08-24 18:48:39 -0400

    📦 deps(patch): upgrade dependencies (#2424)
    Upgrades transitive and non-transitive dependencies
     
    ## Type of change
    
    **PATCH: backwards compatible change**

--- examples/watch/bud.config.ts
+++ examples/watch/bud.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@roots/bud'
 const config: Config = async app => {
   app
     .entry(`app`, [`app.js`, `app.css`])
-    .watch([`./watched`], {usePolling: true, interval: 1000})
+    .watch([`./watched`], {interval: 1000, usePolling: true})
 }
 
 export default config
