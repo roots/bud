cb881996fff151c3ea2662d075ea822183a7d6cd
Kelly Mears <developers@tinypixel.dev>
2023-09-09 03:48:14 -0400

    ⚠️ breaking(major): remove bund and ts-bud

--- sources/@roots/bud/bin/bund.mjs
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env -S bunx
-
-// eslint-disable-next-line n/no-process-env
-process.env.BUD_JS_BIN = `bunx`
-
-import '../lib/cli/index.js'
--- sources/@roots/bud/bin/ts-bud.mjs
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env -S ts-node --esm --transpileOnly --cwdMode
-
-// eslint-disable-next-line n/no-process-env
-process.env.BUD_JS_BIN = `ts-node`
-
-import '../lib/cli/index.js'
