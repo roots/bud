e9282ad1589e7966203f159ab209456a6095eecd
Kelly Mears <developers@tinypixel.dev>
2023-12-16 01:59:38 -0500

    🧹 chore(none): docusaurus@v3 codeblock compatibility

--- sources/@repo/docs/src/components/example.jsx
+++ sources/@repo/docs/src/components/example.jsx
@@ -3,7 +3,7 @@ import TabItem from '@theme/TabItem'
 import Tabs from '@theme/Tabs'
 import React from 'react'
 
-export default function ExampleConfig({children, js, json, ts, yml}) {
+export default function ExampleConfig({children}) {
   if (!children) return null
 
   const values = []
@@ -15,26 +15,24 @@ export default function ExampleConfig({children, js, json, ts, yml}) {
     .map(child => child.props.children.props)
     .map(child => {
       return {
+        ...child,
         code: child.children,
-        language: child.className
-          .split(`language-`)
-          .pop(),
-        title: child.title,
+        language: child.className.split(`language-`).pop(),
+        title: child.metastring.split(`=`).pop(),
       }
     })
 
-  if (children.find(child => [`ts`, `tsx`].includes(child.language))) {
+  children.find(child => [`ts`, `tsx`].includes(child.language)) &&
     values.push({label: `TS`, value: `ts`})
-  }
-  if (children.find(child => [`js`, `jsx`].includes(child.language))) {
+
+  children.find(child => [`js`, `jsx`].includes(child.language)) &&
     values.push({label: `JS`, value: `js`})
-  }
-  if (children.find(child => [`yml`].includes(child.language))) {
+
+  children.find(child => [`yml`].includes(child.language)) &&
     values.push({label: `YML`, value: `yml`})
-  }
-  if (children.find(child => [`json`].includes(child.language))) {
+
+  children.find(child => [`json`].includes(child.language)) &&
     values.push({label: `JSON`, value: `json`})
-  }
 
   return (
     <Tabs groupId="language" values={values}>
@@ -42,13 +40,28 @@ export default function ExampleConfig({children, js, json, ts, yml}) {
         return (
           <TabItem key={id} value={child.language}>
             <CodeBlock
+              className={child.className}
               language={child.language}
+              metastring={child.metastring}
               showLineNumbers
-              title={`${child.title}`}
+              title={child.title}
             >
-              {child.language === `ts` ? `import type {Bud} from '@roots/bud'\n\nexport default async (bud: Bud) => {\n` : ``}
-              {child.language === `js` ? `/** @param {import('@roots/bud').Bud} bud */\nexport default async (bud) => {\n` : ``}
-              {[`js`, `ts`].includes(child.language) ? child.code.split(`\n`).map((line, id) => id + 1 === child.code.split(`\n`).length ? `` : `  ${line}`).join(`\n`) : child.code}
+              {child.language === `ts`
+                ? `import type {Bud} from '@roots/bud'\n\nexport default async (bud: Bud) => {\n`
+                : ``}
+              {child.language === `js`
+                ? `/** @param {import('@roots/bud').Bud} bud */\nexport default async (bud) => {\n`
+                : ``}
+              {[`js`, `ts`].includes(child.language)
+                ? child.code
+                    .split(`\n`)
+                    .map((line, id) =>
+                      id + 1 === child.code.split(`\n`).length
+                        ? ``
+                        : `  ${line}`,
+                    )
+                    .join(`\n`)
+                : child.code}
               {[`js`, `ts`].includes(child.language) ? `}` : ``}
             </CodeBlock>
           </TabItem>
