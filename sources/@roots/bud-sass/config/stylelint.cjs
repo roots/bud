ca2e1594841995cb613a86f6edb1a0484eb30d19
Alex Kozack <cawa-93@users.noreply.github.com>
2024-01-07 08:47:48 +0000

    mode custom-property-pattern into bud-preset-wordpress

--- sources/@roots/bud-sass/config/stylelint.cjs
+++ sources/@roots/bud-sass/config/stylelint.cjs
@@ -3,12 +3,4 @@ module.exports = {
     `@roots/bud-stylelint/config`,
     `stylelint-config-recommended-scss`,
   ],
-  rules: {
-    'custom-property-pattern': [
-      '^([a-z][a-z0-9]*)(-{1,2}[a-z0-9]+)*$',
-      {
-        message: name => `Expected custom property name "${name}" to be kebab-case`,
-      },
-    ],
-  },
 }
