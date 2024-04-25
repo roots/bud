719108513b0f97ca623c5401a3898d473b00d24b
Kelly Mears <developers@tinypixel.dev>
2024-04-24 22:32:13 -0400

    🩹 fix(patch): fix s3 errors (#2574) (#2576)
    - Fix: #2574
    - Add: integration test
    - Add: examples/s3
    
    ## Type of change
    
    **PATCH: backwards compatible change**

---
+++ examples/s3/src/components/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg'
+
+export const App = () => {
+  return (
+    <div className="App">
+      <div className="header">
+        <img src={logo} className="logo" alt="logo" />
+        Edit <code>src/components/App.js</code> and save to reload
+      </div>
+    </div>
+  )
+}
