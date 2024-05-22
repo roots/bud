86ae6a6110127d981574510c80d66cc6a0412a4b
Kelly Mears <developers@tinypixel.dev>
2024-05-22 01:58:30 -0400

    🧪 test(none): fix cli-flag-cache

--- sources/@roots/bud/test/cli-flag-cache/cache-flag.test.ts
+++ sources/@roots/bud/test/cli-flag-cache/cache-flag.test.ts
@@ -2,33 +2,36 @@ import {path} from '@repo/constants'
 import {execa} from 'execa'
 import {beforeEach, describe, expect, it} from 'vitest'
 
-const projectDir = path(
-  `sources`,
-  `@roots`,
-  `bud`,
-  `test`,
-  `cli-flag-cache`,
-  `__fixtures__`,
-)
+const childOptions = {
+  cwd: path(
+    `sources`,
+    `@roots`,
+    `bud`,
+    `test`,
+    `cli-flag-cache`,
+    `__fixtures__`,
+  ),
+  env: {CI: `false`},
+  extendEnv: true,
+}
 
 describe.sequential(`--cache`, async () => {
   beforeEach(async () => {
-    await execa(`yarn`, [`bud`, `clean`, `storage`], {cwd: projectDir})
+    await execa(`yarn`, [`bud`, `clean`, `storage`], childOptions)
   })
 
   it(`should generate cache by default`, async () => {
-    await execa(`yarn`, [`bud`, `build`], {cwd: projectDir})
-    const {stdout} = await execa(`yarn`, [`bud`, `build`], {
-      cwd: projectDir,
-    })
-
+    await execa(`yarn`, [`bud`, `build`], childOptions)
+    const {stdout} = await execa(`yarn`, [`bud`, `build`], childOptions)
     expect(stdout).toMatch(/4\/4 modules cached/)
   })
 
   it(`should not generate cache when --no-cache is passed`, async () => {
-    const {stdout} = await execa(`yarn`, [`bud`, `build`, `--no-cache`], {
-      cwd: projectDir,
-    })
+    const {stdout} = await execa(
+      `yarn`,
+      [`bud`, `build`, `--no-cache`],
+      childOptions,
+    )
     expect(stdout).toMatch(/0\/4 modules cached/)
   })
 })
