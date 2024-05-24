9bee40a6dc89107d4483d6200bd7f6726abcae1e
Kelly Mears <developers@tinypixel.dev>
2024-05-24 01:08:24 -0400

    🧪 test(none): fix e2e test

--- config/vitest/config.e2e.ts
+++ config/vitest/config.e2e.ts
@@ -6,5 +6,12 @@ export default defineConfig({
   test: {
     ...shared,
     include: [`tests/e2e/**/*.test.ts`],
+    poolOptions: {
+      threads: {
+        isolate: false,
+        maxThreads: 1,
+        minThreads: 1,
+      },
+    },
   },
 })
