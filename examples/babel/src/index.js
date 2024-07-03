3da9f7f946b4f6a1f72d2fbc42e4b51067985999
Kelly Mears <developers@tinypixel.dev>
2024-07-03 02:15:14 -0400

    🩹 fix(none): fix @roots/bud-client import error

--- examples/babel/src/index.js
+++ examples/babel/src/index.js
@@ -2,4 +2,8 @@ import '@src/app.css'
 
 document.querySelector('#root')?.classList.add('init')
 
-if (import.meta.webpackHot) import.meta.webpackHot.accept(console.error)
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept(console.error)
+}
+
+export {}
--- sources/@roots/sage/src/client/index.ts
+++ sources/@roots/sage/src/client/index.ts
@@ -1,1 +1,2 @@
-export * from '@roots/bud-client'
+export {default as domReady} from '@roots/bud-client/dom-ready'
+export {default as lazy} from '@roots/bud-client/lazy'
